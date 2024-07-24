820ba69766ac9da3887e9da5b3e842165633c2b2
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-07-24 13:03:42 +0500

    jnjn

--- app/Components/Vehicles.tsx
+++ app/Components/Vehicles.tsx
@@ -26,6 +26,7 @@ import { useState } from "react";
 import GridView from "./GridView";
 export default function Vehicles() {
   const [gridView, setGridView] = useState(false);
+  const [showLess, setShowLess] = useState(true);
   return (
     <div className="nav-width h-full absolute right-0 flex flex-col justify-start items-start gap-[20px] pe-[50px] ps-[40px]">
       <div className="w-full h-[200px bg-yellow-30">
@@ -54,102 +55,108 @@ export default function Vehicles() {
             </button>
           </div>
         </div>
-        <div className="w-full flex justify-between items-center">
-          <div className="w-[18%] h-fit">
-            <h3 className="font-[400] text-[14px] leading-[17px] text-black pb-1 ">
-              Registration No.
-            </h3>
-            <div className="w-full h-fit flex justify-between items-center ">
-              <input
-                className=" font-[400] text-[14px] leading-[18px] px-5 w-[100%] h-[43px] flex justify-between items-center bg-white rounded-xl border-2 border-grey"
-                value={"MBU56i403378"}
-              ></input>
+        {!showLess ? (
+          <div className="w-full flex justify-between items-center">
+            <div className="w-[18%] h-fit">
+              <h3 className="font-[400] text-[14px] leading-[17px] text-black pb-1 ">
+                Registration No.
+              </h3>
+              <div className="w-full h-fit flex justify-between items-center ">
+                <input
+                  className=" font-[400] text-[14px] leading-[18px] px-5 w-[100%] h-[43px] flex justify-between items-center bg-white rounded-xl border-2 border-grey"
+                  value={"MBU56i403378"}
+                ></input>
+              </div>
             </div>
-          </div>
-          <div className="w-[15%] h-fit ">
-            <h3 className="font-[400] text-[14px] leading-[17px] text-black pb-1 ">
-              Make
-            </h3>
-            <div className="w-full h-fit flex justify-between items-center relative overflow-hidden">
-              <select className="pe-10 font-[400] text-[14px] leading-[18px] ps-1 w-[100%] h-[43px] flex justify-between items-center bg-white rounded-xl border-2 border-grey">
-                <option value="">Suzuki</option>
-                <option value="">Suzuki</option>
-                <option value="">Suzuki</option>
-                <option value="">Suzuki</option>
-              </select>
-              <div className="w-[30px] h-[35px] bg-white absolute right-1 rounded-xl flex justify-center items-center pointer-events-none">
-                <img src={shape.src} className="w-[10.5px]" />
+            <div className="w-[15%] h-fit ">
+              <h3 className="font-[400] text-[14px] leading-[17px] text-black pb-1 ">
+                Make
+              </h3>
+              <div className="w-full h-fit flex justify-between items-center relative overflow-hidden">
+                <select className="pe-10 font-[400] text-[14px] leading-[18px] ps-1 w-[100%] h-[43px] flex justify-between items-center bg-white rounded-xl border-2 border-grey">
+                  <option value="">Suzuki</option>
+                  <option value="">Suzuki</option>
+                  <option value="">Suzuki</option>
+                  <option value="">Suzuki</option>
+                </select>
+                <div className="w-[30px] h-[35px] bg-white absolute right-1 rounded-xl flex justify-center items-center pointer-events-none">
+                  <img src={shape.src} className="w-[10.5px]" />
+                </div>
+              </div>
+            </div>
+            <div className="w-[15%] h-fit ">
+              <h3 className="font-[400] text-[14px] leading-[17px] text-black pb-1 ">
+                Model
+              </h3>
+              <div className="w-full h-fit flex justify-between items-center relative overflow-hidden">
+                <select className="pe-10 font-[400] text-[14px] leading-[18px] ps-1 w-[100%] h-[43px] flex justify-between items-center bg-white rounded-xl border-2 border-grey">
+                  <option value="">Swift</option>
+                  <option value="">Swift</option>
+                  <option value="">Swift</option>
+                  <option value="">Swift</option>
+                </select>
+                <div className="w-[30px] h-[35px] bg-white absolute right-1 rounded-xl flex justify-center items-center pointer-events-none">
+                  <img src={shape.src} className="w-[10.5px]" />
+                </div>
+              </div>
+            </div>
+            <div className="w-[15%] h-fit ">
+              <h3 className="font-[400] text-[14px] leading-[17px] text-black pb-1 ">
+                Year
+              </h3>
+              <div className="w-full h-fit flex justify-between items-center relative overflow-hidden">
+                <select className="pe-10 font-[400] text-[14px] leading-[18px] ps-1 w-[100%] h-[43px] flex justify-between items-center bg-white rounded-xl border-2 border-grey">
+                  <option value="">2024</option>
+                  <option value="">2024</option>
+                  <option value="">2024</option>
+                  <option value="">2024</option>
+                </select>
+                <div className="w-[30px] h-[35px] bg-white absolute right-1 rounded-xl flex justify-center items-center pointer-events-none">
+                  <img src={shape.src} className="w-[10.5px]" />
+                </div>
+              </div>{" "}
+            </div>
+            <div className="w-[15%] h-fit ">
+              <h3 className="font-[400] text-[14px] leading-[17px] text-black pb-1 ">
+                Type
+              </h3>
+              <div className="w-full h-fit flex justify-between items-center relative overflow-hidden">
+                <select className="pe-10 font-[400] text-[14px] leading-[18px] ps-1 w-[100%] h-[43px] flex justify-between items-center bg-white rounded-xl border-2 border-grey">
+                  <option value="">Sedan</option>
+                  <option value="">Sedan</option>
+                  <option value="">Sedan</option>
+                  <option value="">Sedan</option>
+                </select>
+                <div className="w-[30px] h-[35px] bg-white absolute right-1 rounded-xl flex justify-center items-center pointer-events-none">
+                  <img src={shape.src} className="w-[10.5px]" />
+                </div>
+              </div>
+            </div>
+            <div className="w-[15%] h-fit ">
+              <h3 className="font-[400] text-[14px] leading-[17px] text-black pb-1 ">
+                Color
+              </h3>
+              <div className="w-full h-fit flex justify-between items-center relative">
+                <select className="ps-6 font-[400] text-[14px] leading-[18px] px-5 w-[100%] h-[43px] flex justify-between items-center bg-white rounded-xl border-2 border-grey">
+                  <option value="">Red</option>
+                  <option value="">Red</option>
+                  <option value="">Red</option>
+                  <option value="">Red</option>
+                </select>
+                <div className=" rounded-full w-[15px] h-[15px] bg-red-500 absolute left-2 top-[30%]"></div>
+                <div className="w-[30px] h-[35px] bg-white absolute right-1 rounded-xl flex justify-center items-center pointer-events-none">
+                  <img src={shape.src} className="w-[10.5px]" />
+                </div>
               </div>
             </div>
           </div>
-          <div className="w-[15%] h-fit ">
-            <h3 className="font-[400] text-[14px] leading-[17px] text-black pb-1 ">
-              Model
-            </h3>
-            <div className="w-full h-fit flex justify-between items-center relative overflow-hidden">
-              <select className="pe-10 font-[400] text-[14px] leading-[18px] ps-1 w-[100%] h-[43px] flex justify-between items-center bg-white rounded-xl border-2 border-grey">
-                <option value="">Swift</option>
-                <option value="">Swift</option>
-                <option value="">Swift</option>
-                <option value="">Swift</option>
-              </select>
-              <div className="w-[30px] h-[35px] bg-white absolute right-1 rounded-xl flex justify-center items-center pointer-events-none">
-                <img src={shape.src} className="w-[10.5px]" />
-              </div>
-            </div>
-          </div>
-          <div className="w-[15%] h-fit ">
-            <h3 className="font-[400] text-[14px] leading-[17px] text-black pb-1 ">
-              Year
-            </h3>
-            <div className="w-full h-fit flex justify-between items-center relative overflow-hidden">
-              <select className="pe-10 font-[400] text-[14px] leading-[18px] ps-1 w-[100%] h-[43px] flex justify-between items-center bg-white rounded-xl border-2 border-grey">
-                <option value="">2024</option>
-                <option value="">2024</option>
-                <option value="">2024</option>
-                <option value="">2024</option>
-              </select>
-              <div className="w-[30px] h-[35px] bg-white absolute right-1 rounded-xl flex justify-center items-center pointer-events-none">
-                <img src={shape.src} className="w-[10.5px]" />
-              </div>
-            </div>{" "}
-          </div>
-          <div className="w-[15%] h-fit ">
-            <h3 className="font-[400] text-[14px] leading-[17px] text-black pb-1 ">
-              Type
-            </h3>
-            <div className="w-full h-fit flex justify-between items-center relative overflow-hidden">
-              <select className="pe-10 font-[400] text-[14px] leading-[18px] ps-1 w-[100%] h-[43px] flex justify-between items-center bg-white rounded-xl border-2 border-grey">
-                <option value="">Sedan</option>
-                <option value="">Sedan</option>
-                <option value="">Sedan</option>
-                <option value="">Sedan</option>
-              </select>
-              <div className="w-[30px] h-[35px] bg-white absolute right-1 rounded-xl flex justify-center items-center pointer-events-none">
-                <img src={shape.src} className="w-[10.5px]" />
-              </div>
-            </div>
-          </div>
-          <div className="w-[15%] h-fit ">
-            <h3 className="font-[400] text-[14px] leading-[17px] text-black pb-1 ">
-              Color
-            </h3>
-            <div className="w-full h-fit flex justify-between items-center relative">
-              <select className="ps-6 font-[400] text-[14px] leading-[18px] px-5 w-[100%] h-[43px] flex justify-between items-center bg-white rounded-xl border-2 border-grey">
-                <option value="">Red</option>
-                <option value="">Red</option>
-                <option value="">Red</option>
-                <option value="">Red</option>
-              </select>
-              <div className=" rounded-full w-[15px] h-[15px] bg-red-500 absolute left-2 top-[30%]"></div>
-              <div className="w-[30px] h-[35px] bg-white absolute right-1 rounded-xl flex justify-center items-center pointer-events-none">
-                <img src={shape.src} className="w-[10.5px]" />
-              </div>
-            </div>
-          </div>
-        </div>
-        <h3 className="font-[400] text-[14px] leading-[17px] text-black pb-1 underline">
-          Show Less
+        ) : null}
+
+        <h3
+          className="font-[400] text-[14px] leading-[17px] text-black pb-1 underline hover:no-underline cursor-pointer"
+          onClick={() => setShowLess(!showLess)}
+        >
+          {showLess ? "Advanced Features" : "Show Less"}
         </h3>
       </div>
       <div className="w-full h-fit">
