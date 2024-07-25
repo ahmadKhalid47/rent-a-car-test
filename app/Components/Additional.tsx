1dd259770d23dcee1b89cc605e61bdfad128b736
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-07-25 09:45:30 +0500

    kmlmk

--- app/Components/Additional.tsx
+++ app/Components/Additional.tsx
@@ -4,22 +4,22 @@ import ft3 from "@/public/ft (3).svg";
 
 export default function Additional() {
   return (
-    <div className="w-full h-fit flex justify-between flex-wrap items-start gap-x-[5%] gap-y-[5%] py-7 px-6 rounded-[10px] bg-light-grey border-2 border-grey bg-light-grey mt-5">
+    <div className="w-full h-fit flex justify-between flex-wrap items-start gap-x-[5%] gap-y-[5%] py-5 px-6 rounded-[10px] bg-light-grey border-2 border-grey bg-light-grey mt-5">
       <div className="w-[25%] h-fit flex gap-2 justify-center items-start bg-red-30">
         <img src={ft1.src} />
-        <p className="w-[35%] text-start font-[400] text-[18px] leading-[21px]">
+        <p className="w-[35%] text-start font-[400] text-[18px] leading-[21px] mt-1">
           GPS
         </p>
       </div>
       <div className="w-[25%] h-fit flex gap-2 justify-center items-start bg-red-30">
         <img src={ft3.src} />
-        <p className="w-fit text-start font-[400] text-[18px] leading-[21px]">
+        <p className="w-fit text-start font-[400] text-[18px] leading-[21px] mt-[2px]">
           Air Conditioning
         </p>
       </div>
       <div className="w-[25%] h-fit flex gap-2 justify-center items-start bg-red-30">
         <img src={ft2.src} />
-        <p className="w-fit text-start font-[400] text-[18px] leading-[21px]">
+        <p className="w-fit text-start font-[400] text-[18px] leading-[21px] mt-[2px]">
           Bluetooth
         </p>
       </div>
--- app/Components/Rental.tsx
+++ app/Components/Rental.tsx
@@ -1,6 +1,6 @@
 export default function Rental() {
   return (
-    <div className="w-full h-fit flex justify-between flex-wrap items-start gap-x-[5%] gap-y-[5%] py-7 px-6 rounded-[10px] bg-light-grey border-2 border-grey bg-light-grey mt-5">
+    <div className="w-full h-fit flex justify-between flex-wrap items-start gap-x-[5%] gap-y-[5%] py-5 px-6 rounded-[10px] bg-light-grey border-2 border-grey bg-light-grey mt-5">
       <div className="w-[35%] h-fit flex flex-col justify-start items-start bg-red-30">
         <div className="w-full h-fit flex justify-between items-start py-[3px]">
           <p className="font-[400] text-[18px] leading-[27px]">
