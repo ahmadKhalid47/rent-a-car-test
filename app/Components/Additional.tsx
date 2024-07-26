a5d11eca696f0fed60203900f624b558fa54076f
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-07-26 19:06:26 +0500

    fvd

--- app/Components/Additional.tsx
+++ app/Components/Additional.tsx
@@ -4,24 +4,17 @@ import ft3 from "@/public/ft (3).svg";
 
 export default function Additional() {
   return (
-    <div className="w-full h-fit flex justify-between flex-wrap items-start gap-x-[5%] gap-y-[5%] py-5 px-6 rounded-[10px] bg-light-grey border-2 border-grey bg-light-grey mt-5">
-      <div className="w-[25%] h-fit flex gap-2 justify-center items-start bg-red-30">
-        <img src={ft1.src} />
-        <p className="w-[35%] text-start font-[400] text-[18px] leading-[21px] mt-1">
-          GPS
-        </p>
+    <div className="w-[50%] h-fit flex justify-between flex-wrap items-start gap-x-[5%] gap-y-[5%] py-5 px-6 rounded-[10px] bg-light-grey border-2 border-grey bg-light-grey mt-5">
+      <div className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px]">
+        <p className="font-[600] text-[18px] leading-[27px]">GPS</p>
       </div>
-      <div className="w-[25%] h-fit flex gap-2 justify-center items-start bg-red-30">
-        <img src={ft3.src} />
-        <p className="w-fit text-start font-[400] text-[18px] leading-[21px] mt-[2px]">
+      <div className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px]">
+        <p className="font-[600] text-[18px] leading-[27px]">
           Air Conditioning
         </p>
       </div>
-      <div className="w-[25%] h-fit flex gap-2 justify-center items-start bg-red-30">
-        <img src={ft2.src} />
-        <p className="w-fit text-start font-[400] text-[18px] leading-[21px] mt-[2px]">
-          Bluetooth
-        </p>
+      <div className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px]">
+        <p className="font-[600] text-[18px] leading-[27px]">Bluetooth</p>
       </div>
     </div>
   );
