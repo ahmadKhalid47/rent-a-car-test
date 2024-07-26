629b334f4ab57e53e82e3eed6c4d12e368064cb2
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-07-26 10:33:50 +0500

    fvsdfv

--- app/Components/GridView.tsx
+++ app/Components/GridView.tsx
@@ -48,6 +48,126 @@ export default function GridView() {
           </div>
         </div>
       </Link>
+      <Link
+        href={"/Components/CarInfo"}
+        className="w-[47%] h-[183px bg-white mt-[5%] rounded-[15px] shadow px-5 py-6 flex justify-between items-center relative"
+      >
+        <div className="absolute top-5 right-5 rotate-90 hover:cursor-pointer">
+          <FaEllipsis />
+        </div>
+        <div className="w-[170px] h-[139px] overflow-hidden rounded-[15px]">
+          <img src={car2.src} className="w-full h-full" />
+        </div>
+        <div className="w-[55%] h-fit flex justify-start flex-wrap items-center gap-1">
+          <div className="w-full flex justify-start items-center pe-5 -mb-1">
+            <p className="font-[500] text-[24px] leading-[36px]">Honda City</p>
+          </div>
+          <div className="w-full flex justify-start items-center">
+            <p className="font-[400] text-[14px] leading-[21px]">LEM 1234</p>
+          </div>
+          <div className="w-full flex justify-between items-center">
+            <div className="flex justify-start items-center gap-2 w-[40%] pe-5">
+              <p className="font-[400] text-[12px] leading-[18px]">Year:</p>
+              <p className="font-[600] text-[13px] leading-[15px]">2024</p>
+            </div>
+            <div className="flex justify-start items-center gap-2 w-[50%]">
+              <p className="font-[400] text-[12px] leading-[18px]">Type:</p>
+              <p className="font-[600] text-[13px] leading-[15px]">539485</p>
+            </div>
+          </div>
+
+          <div className="w-full flex justify-between items-center">
+            <div className="flex justify-start items-center gap-2 w-[40%]">
+              <p className="font-[400] text-[12px] leading-[18px]">Color:</p>
+              <p className="font-[600] text-[13px] leading-[15px]">White</p>
+            </div>
+            <div className="flex justify-start items-center gap-2 w-[50%]">
+              <p className="font-[400] text-[12px] leading-[18px]">City:</p>
+              <p className="font-[600] text-[13px] leading-[15px]">Petrol</p>
+            </div>
+          </div>
+        </div>
+      </Link>
+      <Link
+        href={"/Components/CarInfo"}
+        className="w-[47%] h-[183px bg-white mt-[5%] rounded-[15px] shadow px-5 py-6 flex justify-between items-center relative"
+      >
+        <div className="absolute top-5 right-5 rotate-90 hover:cursor-pointer">
+          <FaEllipsis />
+        </div>
+        <div className="w-[170px] h-[139px] overflow-hidden rounded-[15px]">
+          <img src={car2.src} className="w-full h-full" />
+        </div>
+        <div className="w-[55%] h-fit flex justify-start flex-wrap items-center gap-1">
+          <div className="w-full flex justify-start items-center pe-5 -mb-1">
+            <p className="font-[500] text-[24px] leading-[36px]">Honda City</p>
+          </div>
+          <div className="w-full flex justify-start items-center">
+            <p className="font-[400] text-[14px] leading-[21px]">LEM 1234</p>
+          </div>
+          <div className="w-full flex justify-between items-center">
+            <div className="flex justify-start items-center gap-2 w-[40%] pe-5">
+              <p className="font-[400] text-[12px] leading-[18px]">Year:</p>
+              <p className="font-[600] text-[13px] leading-[15px]">2024</p>
+            </div>
+            <div className="flex justify-start items-center gap-2 w-[50%]">
+              <p className="font-[400] text-[12px] leading-[18px]">Type:</p>
+              <p className="font-[600] text-[13px] leading-[15px]">539485</p>
+            </div>
+          </div>
+
+          <div className="w-full flex justify-between items-center">
+            <div className="flex justify-start items-center gap-2 w-[40%]">
+              <p className="font-[400] text-[12px] leading-[18px]">Color:</p>
+              <p className="font-[600] text-[13px] leading-[15px]">White</p>
+            </div>
+            <div className="flex justify-start items-center gap-2 w-[50%]">
+              <p className="font-[400] text-[12px] leading-[18px]">City:</p>
+              <p className="font-[600] text-[13px] leading-[15px]">Petrol</p>
+            </div>
+          </div>
+        </div>
+      </Link>
+      <Link
+        href={"/Components/CarInfo"}
+        className="w-[47%] h-[183px bg-white mt-[5%] rounded-[15px] shadow px-5 py-6 flex justify-between items-center relative"
+      >
+        <div className="absolute top-5 right-5 rotate-90 hover:cursor-pointer">
+          <FaEllipsis />
+        </div>
+        <div className="w-[170px] h-[139px] overflow-hidden rounded-[15px]">
+          <img src={car2.src} className="w-full h-full" />
+        </div>
+        <div className="w-[55%] h-fit flex justify-start flex-wrap items-center gap-1">
+          <div className="w-full flex justify-start items-center pe-5 -mb-1">
+            <p className="font-[500] text-[24px] leading-[36px]">Honda City</p>
+          </div>
+          <div className="w-full flex justify-start items-center">
+            <p className="font-[400] text-[14px] leading-[21px]">LEM 1234</p>
+          </div>
+          <div className="w-full flex justify-between items-center">
+            <div className="flex justify-start items-center gap-2 w-[40%] pe-5">
+              <p className="font-[400] text-[12px] leading-[18px]">Year:</p>
+              <p className="font-[600] text-[13px] leading-[15px]">2024</p>
+            </div>
+            <div className="flex justify-start items-center gap-2 w-[50%]">
+              <p className="font-[400] text-[12px] leading-[18px]">Type:</p>
+              <p className="font-[600] text-[13px] leading-[15px]">539485</p>
+            </div>
+          </div>
+
+          <div className="w-full flex justify-between items-center">
+            <div className="flex justify-start items-center gap-2 w-[40%]">
+              <p className="font-[400] text-[12px] leading-[18px]">Color:</p>
+              <p className="font-[600] text-[13px] leading-[15px]">White</p>
+            </div>
+            <div className="flex justify-start items-center gap-2 w-[50%]">
+              <p className="font-[400] text-[12px] leading-[18px]">City:</p>
+              <p className="font-[600] text-[13px] leading-[15px]">Petrol</p>
+            </div>
+          </div>
+        </div>
+      </Link>
     </div>
   );
 }
