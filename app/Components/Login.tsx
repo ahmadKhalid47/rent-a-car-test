57eef0bbd785fe025a5251b6a7e600afe303c8ed
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-07-26 14:43:51 +0500

    fvjdfnvkd

--- app/Components/Login.tsx
+++ app/Components/Login.tsx
@@ -22,15 +22,22 @@ export default function Login() {
             className="w-[100%] h-[50%] absolute bottom-0 left-0"
           />
 
-          <div className="w-fit bg-red-30 h-fit flex flex-col justify-center items-start gap-[31px]">
+          <div className="w-fit bg-red-30 h-fit flex flex-col justify-center items-start gap-[20px]">
             <img src={car.src} className="w-[175px] z-10" />
             <h1 className="font-[600] text-[70px] leading-[73px] capitalize text-white">
               welcome <br /> back!
             </h1>
             <p className="font-[400] text-[18px] leading-[22px] text-white z-10 ">
-              Ready to manage your fleet and reservations? Let's keep
-              <br />
-               things running smoothly together.{" "}
+              {/* Ready to manage your fleet and reservations? Let's keep
+               things running smoothly together.{" "} */}
+              Are you ready to efficiently manage your fleet and
+               <br />
+              reservations?
+              Let's work together to ensure everything
+               <br />
+              runs
+              smoothly and
+              seamlessly.
             </p>
           </div>
         </div>
