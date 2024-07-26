c28f5995cd994ed7bcc34466ea9d67b7428a31ec
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-07-26 18:08:27 +0500

    vfdv

--- app/Components/General.tsx
+++ app/Components/General.tsx
@@ -1,28 +1,22 @@
 export default function General() {
   return (
-    <div className="w-full h-fit flex justify-between flex-wrap items-start gap-x-[5%] gap-y-[5%] py-5 px-6 rounded-[10px] bg-light-grey border-2 border-grey bg-light-grey mt-5">
-      <div className="w-[35%] h-fit flex flex-col justify-start items-start bg-red-30">
+    <div className="w-[40%] h-fit flex justify-between flex-wrap items-start gap-x-[5%] gap-y-[5%] py-5 px-6 rounded-[10px] bg-light-grey border-2 border-grey bg-light-grey mt-5">
+      <div className="w-[100%] h-fit flex flex-col justify-start items-start bg-red-30 ">
         <div className="w-full h-fit flex justify-between items-start py-[3px]">
-          <p className="font-[400] text-[18px] leading-[27px]">Vehicle Id:</p>
-          <p className="w-[21%] text-start font-[400] text-[18px] leading-[27px]">
-            539485
-          </p>
-        </div>
-        <div className="w-full h-fit flex justify-between items-start py-[3px] border-t-[2px]">
           <p className="font-[400] text-[18px] leading-[27px]">Make:</p>
-          <p className="w-[21%] text-start font-[400] text-[18px] leading-[27px]">
+          <p className="w-[30%] text-start font-[400] text-[18px] leading-[27px]">
             Suzuki
           </p>
         </div>
         <div className="w-full h-fit flex justify-between items-start py-[3px] border-t-[2px]">
           <p className="font-[400] text-[18px] leading-[27px]">Year:</p>
-          <p className="w-[21%] text-start font-[400] text-[18px] leading-[27px]">
+          <p className="w-[30%] text-start font-[400] text-[18px] leading-[27px]">
             2024
           </p>
         </div>
         <div className="w-full h-fit flex justify-between items-start py-[3px] border-t-[2px]">
           <p className="font-[400] text-[18px] leading-[27px]">Color:</p>
-          <p className="w-[21%] text-start font-[400] text-[18px] leading-[27px]">
+          <p className="w-[30%] text-start font-[400] text-[18px] leading-[27px]">
             Red
           </p>
         </div>
@@ -30,35 +24,27 @@ export default function General() {
           <p className="font-[400] text-[18px] leading-[27px]">
             Transformation:
           </p>
-          <p className="w-[21%] text-start font-[400] text-[18px] leading-[27px]">
+          <p className="w-[30%] text-start font-[400] text-[18px] leading-[27px]">
             Auto
           </p>
         </div>
-      </div>
-      <div className="w-[35%] h-fit flex flex-col justify-start items-start bg-red-30 me-10">
-        <div className="w-full h-fit flex justify-between items-start py-[3px]">
-          <p className="font-[400] text-[18px] leading-[27px]">
-            Registration No:
-          </p>
-          <p className="w-[27%] text-start font-[400] text-[18px] leading-[27px]">
-            LMS 6496
+        <div className="w-full h-fit flex justify-between items-start py-[3px] border-t-[2px]">
+          <p className="font-[400] text-[18px] leading-[27px]">Vehicle Id:</p>
+          <p className="w-[30%] text-start font-[400] text-[18px] leading-[27px]">
+            539485
           </p>
         </div>
+
         <div className="w-full h-fit flex justify-between items-start py-[3px] border-t-[2px]">
           <p className="font-[400] text-[18px] leading-[27px]">Model:</p>
-          <p className="w-[27%] text-start font-[400] text-[18px] leading-[27px]">
+          <p className="w-[30%] text-start font-[400] text-[18px] leading-[27px]">
             Swift
           </p>
         </div>
-        <div className="w-full h-fit flex justify-between items-start py-[3px] border-t-[2px]">
-          <p className="font-[400] text-[18px] leading-[27px]">Type:</p>
-          <p className="w-[27%] text-start font-[400] text-[18px] leading-[27px]">
-            Sedan
-          </p>
-        </div>
+
         <div className="w-full h-fit flex justify-between items-start py-[3px] border-t-[2px]">
           <p className="font-[400] text-[18px] leading-[27px]">Fuel Type:</p>
-          <p className="w-[27%] text-start font-[400] text-[18px] leading-[27px]">
+          <p className="w-[30%] text-start font-[400] text-[18px] leading-[27px]">
             Petrol
           </p>
         </div>
@@ -66,7 +52,7 @@ export default function General() {
           <p className="font-[400] text-[18px] leading-[27px]">
             Current Millage:
           </p>
-          <p className="w-[27%] text-start font-[400] text-[18px] leading-[27px]">
+          <p className="w-[30%] text-start font-[400] text-[18px] leading-[27px]">
             5309502
           </p>
         </div>
