b0f98c531ea159fa92f28b973e5fed74c89071cd
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-07-26 12:59:31 +0500

    vfds

--- app/Components/Login.tsx
+++ app/Components/Login.tsx
@@ -28,9 +28,9 @@ export default function Login() {
               welcome <br /> back!
             </h1>
             <p className="font-[400] text-[18px] leading-[22px] text-white z-10 ">
-              Ready to manage your fleet and reservations?
+              Ready to manage your fleet and reservations? Let's keep
               <br />
-              Let's keep things running smoothly together.{" "}
+               things running smoothly together.{" "}
             </p>
           </div>
         </div>
