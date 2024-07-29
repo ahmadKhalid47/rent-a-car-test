037bb5203863fd384b11019ff5d77a27e7c37e97
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-07-29 17:53:21 +0500

    vjnfj

--- app/Components/VehicleForms/Rental.tsx
+++ app/Components/VehicleForms/Rental.tsx
@@ -15,7 +15,50 @@ export default function Rental() {
   return (
     <div className="w-full h-fit  ">
       <div className="flex flex-wrap justify-start items-start gap-x-[4%] gap-y-5 w-full h-fit bg-white mt-5 rounded-[10px] border-2 border-grey px-10 pb-5 pt-7">
-        Rental{" "}
+        <div className="w-[22%] h-fit bg-red-30 flex flex-col justify-start items-start gap-1">
+          <label className="flex justify-start gap-1 items-start font-[400] text-[14px] leading-[17px]">
+            Rental Price Per Hour{" "}
+          </label>
+          <div className="w-full h-fit flex justify-between items-center relative overflow-hidden">
+            <input
+              className="pe-10 font-[400] text-[16px] leading-[19px] ps-2 w-[100%] h-[43px] flex justify-between items-center bg-white rounded-xl border-2 border-grey"
+              value={"$90"}
+            />
+          </div>
+        </div>{" "}
+        <div className="w-[22%] h-fit bg-red-30 flex flex-col justify-start items-start gap-1">
+          <label className="flex justify-start gap-1 items-start font-[400] text-[14px] leading-[17px]">
+            Rental Price Per Day{" "}
+          </label>
+          <div className="w-full h-fit flex justify-between items-center relative overflow-hidden">
+            <input
+              className="pe-10 font-[400] text-[16px] leading-[19px] ps-2 w-[100%] h-[43px] flex justify-between items-center bg-white rounded-xl border-2 border-grey"
+              value={"$150"}
+            />
+          </div>
+        </div>{" "}
+        <div className="w-[22%] h-fit bg-red-30 flex flex-col justify-start items-start gap-1">
+          <label className="flex justify-start gap-1 items-start font-[400] text-[14px] leading-[17px]">
+            Rental Price Per Week{" "}
+          </label>
+          <div className="w-full h-fit flex justify-between items-center relative overflow-hidden">
+            <input
+              className="pe-10 font-[400] text-[16px] leading-[19px] ps-2 w-[100%] h-[43px] flex justify-between items-center bg-white rounded-xl border-2 border-grey"
+              value={"$150"}
+            />
+          </div>
+        </div>{" "}
+        <div className="w-[22%] h-fit bg-red-30 flex flex-col justify-start items-start gap-1">
+          <label className="flex justify-start gap-1 items-start font-[400] text-[14px] leading-[17px]">
+            Rental Price Per Month
+          </label>
+          <div className="w-full h-fit flex justify-between items-center relative overflow-hidden">
+            <input
+              className="pe-10 font-[400] text-[16px] leading-[19px] ps-2 w-[100%] h-[43px] flex justify-between items-center bg-white rounded-xl border-2 border-grey"
+              value={"$400"}
+            />
+          </div>
+        </div>{" "}
       </div>
     </div>
   );
