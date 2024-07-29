ef407d634391fb237b4d13feb2a46a1bea439d8c
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-07-29 16:36:10 +0500

    jjnjn

--- app/Components/Rental.tsx
+++ app/Components/Rental.tsx
@@ -7,7 +7,7 @@ export default function Rental() {
             Rental Price Per Hour:
           </p>
           <p className="w-[17%] text-start font-[400] text-[18px] leading-[27px]">
-            8$
+            $8
           </p>
         </div>
 
@@ -15,9 +15,10 @@ export default function Rental() {
           <p className="font-[400] text-[18px] leading-[27px]">
             Rental Price Per Week:
           </p>
-          <p className="w-[17%] text-start font-[400] text-[18px] leading-[27px]">150$</p>
+          <p className="w-[17%] text-start font-[400] text-[18px] leading-[27px]">
+            $150
+          </p>
         </div>
-     
       </div>
       <div className="w-[37%] h-fit flex flex-col justify-start items-start bg-red-30 ">
         <div className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px]">
@@ -25,7 +26,7 @@ export default function Rental() {
             Rental Price Per Day:
           </p>
           <p className="w-[17%] text-start font-[400] text-[18px] leading-[27px]">
-            90$
+            $90
           </p>
         </div>
 
@@ -33,12 +34,11 @@ export default function Rental() {
           <p className="font-[400] text-[18px] leading-[27px]">
             Rental Price Per Month:
           </p>
-          <p className="w-[17%] text-start font-[400] text-[18px] leading-[27px]">400$</p>
+          <p className="w-[17%] text-start font-[400] text-[18px] leading-[27px]">
+            $400
+          </p>
         </div>
-     
       </div>
-    
     </div>
-
   );
 }
