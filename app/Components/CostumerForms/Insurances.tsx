a714f1b1532c9daad843e9c30c98160c6db4e261
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-07-30 18:23:40 +0500

    vfdsv

--- app/Components/CostumerForms/Insurances.tsx
+++ app/Components/CostumerForms/Insurances.tsx
@@ -17,34 +17,34 @@ export default function Insurances() {
       <div className="flex flex-wrap justify-start items-start gap-x-[4%] gap-y-5 w-full h-fit bg-white mt-5 rounded-[10px] border-2 border-grey px-10 pb-5 pt-7">
         <div className="w-[22%] h-fit bg-red-30 flex flex-col justify-start items-start gap-1">
           <label className="flex justify-start gap-1 items-start font-[400] text-[14px] leading-[17px]">
-            Insurance Policy No
+            Emergency Contact Name
           </label>
           <div className="w-full h-fit flex justify-between items-center relative overflow-hidden">
             <input
-              className="pe-10 font-[400] text-[16px] leading-[19px] ps-2 w-[100%] h-[43px] flex justify-between items-center bg-white rounded-xl border-2 border-grey"
-              value={8694039675}
+              className="pe-10 font-[400] text-[16px] leading-[19px] ps-2 w-[100%] h-[43px] flex justify-between items-center input-color rounded-xl border-2 border-grey"
+              value={"James"}
             />
           </div>
         </div>{" "}
         <div className="w-[22%] h-fit bg-red-30 flex flex-col justify-start items-start gap-1">
           <label className="flex justify-start gap-1 items-start font-[400] text-[14px] leading-[17px]">
-            Insurance Provider
+            Relationship
           </label>
           <div className="w-full h-fit flex justify-between items-center relative overflow-hidden">
             <input
-              className="pe-10 font-[400] text-[16px] leading-[19px] ps-2 w-[100%] h-[43px] flex justify-between items-center bg-white rounded-xl border-2 border-grey"
-              value={"Provider Name"}
+              className="pe-10 font-[400] text-[16px] leading-[19px] ps-2 w-[100%] h-[43px] flex justify-between items-center input-color rounded-xl border-2 border-grey"
+              value={"Brother"}
             />
           </div>
         </div>{" "}
         <div className="w-[22%] h-fit bg-red-30 flex flex-col justify-start items-start gap-1">
           <label className="flex justify-start gap-1 items-start font-[400] text-[14px] leading-[17px]">
-            Insurance Expiry Date
+            Emergency Phone
           </label>
           <div className="w-full h-fit flex justify-between items-center relative overflow-hidden">
             <input
-              type="date"
-              className="pe- font-[400] text-[16px] leading-[19px] ps-2 w-[100%] h-[43px] flex justify-between items-center bg-white rounded-xl border-2 border-grey"
+              value={"59034"}
+              className="pe- font-[400] text-[16px] leading-[19px] ps-2 w-[100%] h-[43px] flex justify-between items-center input-color rounded-xl border-2 border-grey"
             />
           </div>
         </div>{" "}
