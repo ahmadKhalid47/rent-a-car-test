00bbe1222b7c8ab6c47658ebdd39ac739854ab4d
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-08-01 19:13:22 +0500

    njkfnjkfn

--- app/Components/Sidebar.tsx
+++ app/Components/Sidebar.tsx
@@ -14,6 +14,7 @@ import { setFieldNameR, setSidebarShowR } from "../store/Global";
 import { useDispatch } from "react-redux";
 import { GoTriangleDown, GoTriangleUp } from "react-icons/go";
 import { useRouter } from "next/navigation";
+import { FaUserTie } from "react-icons/fa6";
 
 export default function Sidebar() {
   let global = useSelector((state: RootState) => state.Global);
@@ -108,10 +109,10 @@ export default function Sidebar() {
           } rounded-[10px]`}
         >
           <div className="w-fit flex justify-start items-center gap-2 bg-red-30">
-            <FaUsers
+            <FaUserTie
               className={`${global.sidebarShow ? "ml-[1px]" : "ml-[-6px]"}`}
             />
-            {global.sidebarShow ? "Customers" : null}
+            {global.sidebarShow ? "Chauffeurs" : null}
           </div>
           {global.sidebarShow ? (
             <div
@@ -152,7 +153,7 @@ export default function Sidebar() {
                     : ""
                 } hover:text-white rounded-[10px]`}
               >
-                {global.sidebarShow ? "All Costumers" : null}
+                {global.sidebarShow ? "All Chauffeurs" : null}
               </button>{" "}
             </div>
             <button
