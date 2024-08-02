210872b1547048c5131bf7f7d4dc274f36089f1f
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-08-02 19:14:05 +0500

    scds

--- app/Components/ReservationsForms/Feature.tsx
+++ app/Components/ReservationsForms/Feature.tsx
@@ -1,9 +1,115 @@
 "use client";
+import car from "@/public/carInfoCar.svg";
 
 export default function Feature() {
   return (
     <div className="w-full h-full">
-      <div className="flex flex-wrap justify-start items-start gap-x-[4%] gap-y-5 w-full h-full bg-white mt-5 rounded-[10px] border-2 border-grey px-10 py-8"></div>
+      <div className="flex flex-col justify-start items-start gap-x-[4%] gap-y-5 w-full h-full bg-white mt-5 rounded-[10px] border-2 border-grey px-10 py-8 overflow-auto">
+        <div className="w-full flex flex-wrap justify-center items-center h-fit gap-1">
+          <span className="w-full text-start font-[400] text-[14px] leading-[17px]">
+            Search Customer
+          </span>
+          <input
+            className="w-full h-[43px] flex justify-start ps-5 items-center border-[1px] border-grey rounded-[10px] input-color text-[16px] leading-[19px] placeholder:text-black"
+            placeholder="Search"
+          />
+        </div>
+
+        <div className="w-[100%] rounded-[15px] shadow px-5 py-6 flex justify-start gap-4 items-center relative">
+          <div className="w-[133px] overflow-hidden rounded-[10px] border-[1px] border-grey">
+            <img src={car.src} className="w-full h-full" />
+          </div>
+          <div className="w-[55%] h-fit flex justify-start flex-col bg-red-20 items-start">
+            <div className="w-full flex justify-start items-center pe-5 h-fit py-[-10px] -mt-2">
+              <span className="font-[600] text-[24px] leading-[36px]">
+                Suzuki Swift{" "}
+              </span>
+            </div>
+            <div className="w-full flex justify-start items-center py-[-10px] -mt-1">
+              <span className="font-[500] text-[20px] leading-[30px]">
+                LEM 1234{" "}
+              </span>
+            </div>
+
+            <div className="w-full flex justify-start gap-0 items-center font-[400] text-[14px] leading-[21px] -mt-1">
+              <div className="flex justify-start items-center gap-2 w-[40%] pe-5">
+                <span className=" w-[42%]">Year:</span>
+                <span className=" w-[40%]">2024</span>
+              </div>
+              <div className="flex justify-start items-center gap- w-[50%]">
+                <span className=" w-[30%]">Type:</span>
+                <span className=" w-[30%]">Sedan</span>
+              </div>
+            </div>
+
+            <div className="w-full flex justify-start gap-0 items-center font-[400] text-[14px] leading-[21px] -mt-1">
+              <div className="flex justify-start items-center gap-2 w-[40%]">
+                <span className="">Color:</span>
+                <div className="">
+                  <div className="w-[23px] h-[12px] bg-red-600 rounded-full"></div>
+                </div>
+              </div>
+              <div className="flex justify-start items-center gap- w-[50%]">
+                <span className=" w-[30%]">City:</span>
+                <span className=" w-[30%]">Atlanta</span>
+              </div>
+            </div>
+            <div className="w-[77px] flex justify-center items-center h-[22px] border-[1px] text-[12px] leading-[14px] text-center rounded-[5px] complete-status font-[600] mt-1">
+              Available
+            </div>
+          </div>
+          <button className="w-[103px] h-[30px] rounded-[10px] bg-main-blue text-white  font-[500] text-[18px] leading-[21px] text-center">
+            Select
+          </button>
+        </div>
+        <div className="w-[100%] rounded-[15px] shadow px-5 py-6 flex justify-start gap-4 items-center relative">
+          <div className="w-[133px] overflow-hidden rounded-[10px] border-[1px] border-grey">
+            <img src={car.src} className="w-full h-full" />
+          </div>
+          <div className="w-[55%] h-fit flex justify-start flex-col bg-red-20 items-start">
+            <div className="w-full flex justify-start items-center pe-5 h-fit py-[-10px] -mt-2">
+              <span className="font-[600] text-[24px] leading-[36px]">
+                Suzuki Swift{" "}
+              </span>
+            </div>
+            <div className="w-full flex justify-start items-center py-[-10px] -mt-1">
+              <span className="font-[500] text-[20px] leading-[30px]">
+                LEM 1234{" "}
+              </span>
+            </div>
+
+            <div className="w-full flex justify-start gap-0 items-center font-[400] text-[14px] leading-[21px] -mt-1">
+              <div className="flex justify-start items-center gap-2 w-[40%] pe-5">
+                <span className=" w-[42%]">Year:</span>
+                <span className=" w-[40%]">2024</span>
+              </div>
+              <div className="flex justify-start items-center gap- w-[50%]">
+                <span className=" w-[30%]">Type:</span>
+                <span className=" w-[30%]">Sedan</span>
+              </div>
+            </div>
+
+            <div className="w-full flex justify-start gap-0 items-center font-[400] text-[14px] leading-[21px] -mt-1">
+              <div className="flex justify-start items-center gap-2 w-[40%]">
+                <span className="">Color:</span>
+                <div className="">
+                  <div className="w-[23px] h-[12px] bg-red-600 rounded-full"></div>
+                </div>
+              </div>
+              <div className="flex justify-start items-center gap- w-[50%]">
+                <span className=" w-[30%]">City:</span>
+                <span className=" w-[30%]">Atlanta</span>
+              </div>
+            </div>
+            <div className="w-[100px] flex justify-center items-center h-[22px] border-[1px] text-[12px] leading-[14px] text-center rounded-[5px] progress-status font-[600] mt-1">
+              Not Available
+            </div>
+          </div>
+          <button className="w-[103px] h-[30px] rounded-[10px] bg-main-blue text-white  font-[500] text-[18px] leading-[21px] text-center">
+            Select
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- app/Components/ReservationsForms/Rental.tsx
+++ app/Components/ReservationsForms/Rental.tsx
@@ -171,6 +171,7 @@ export default function Rental() {
           </button>
         </div>
       </div>
+
     </div>
   );
 }
