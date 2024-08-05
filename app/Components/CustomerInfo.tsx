900cb1feef679bde21e20016ae30c8efa2137fa1
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-08-05 09:57:33 +0500

    vsf

--- app/Components/CustomerInfo.tsx
+++ app/Components/CustomerInfo.tsx
@@ -45,37 +45,40 @@ export default function CustomerInfo() {
         </div>
       </div>
       <div className="w-full h-fit flex justify-center flex-wrap items-start gap-x-[5%] gap-y-[5%] py-7 px-6 rounded-[10px] border-2 border-grey bg-light-grey mt-5">
-        <div className="w-full h-fit flex justify-start flex-col items-start gap-x-[5%] gap-y-[5%]  rounded-[10px] bg-">
-          <div className="w-full h-fit flex justify-start gap-[7%] items-center px- bg-white rounded-[10px] border-2 border-grey py-7 px-6 ">
+        <div className="w-full h-fit flex justify-start flex-col items-start gap-x-[5%] gap-y-[5%] rounded-[10px] bg-">
+          <div className="w-full h-fit flex flex-col lg:flex-row justify-start gap-5 lg:gap-[7%] items-center px- g-white rounded-[10px] border-2 border-grey py-7 px-6 ">
             <div className="w-fit flex justify-start items-center gap-1">
               <div className="w-[464] h-[464] flex justify-between items-start rounded-[10px] overflow-hidden border-[1px] border-grey bg-white ms-1">
                 <img src={car.src} className="w-full h-full" />
               </div>
             </div>
-            <div className="w-[35%] flex justify-start flex-col items-start gap-1 bg-green-">
+            <div className="w-full lg:w-[60%] 1400:w-[35%] flex justify-start flex-col items-center lg:items-start gap-1 bg-green-">
               <h3 className="font-[600] text-[36px] flex justify-start items-center gap-4 leading-[54px] text-black">
                 Glenn A. Jean{" "}
-                <img src={vip.src} className="w-[44px] h-[32px] -translate-y-1" />
+                <img
+                  src={vip.src}
+                  className="w-[44px] h-[32px] -translate-y-1"
+                />
               </h3>
               <p className="font-[400] text-[28px] leading-[42px] text-black">
                 757-947-5015{" "}
               </p>
-              <div className="w-[80%] flex justify-between items-center">
-                <div className="flex justify-start items-center gap-0 w-[70%] pe-5">
-                  <p className="font-[400] text-[20px] leading-[30px] w-[50%]">
+              <div className="w-full lg:w-[80%] flex justify-center lg:justify-start items-center">
+                <div className="flex justify-start items-center gap-3 lg:gap-0 w-fit lg:w-[70%] pe-5">
+                  <p className="font-[400] text-[20px] leading-[30px] w-full lg:w-[50%]">
                     City:
                   </p>
-                  <p className="font-[400] text-[20px] leading-[30px] w-[50%]">
+                  <p className="font-[400] text-[20px] leading-[30px] w-full lg:w-[50%]">
                     Brentwood
                   </p>
                 </div>
               </div>
-              <div className="w-[80%] flex justify-between items-center bg-red-5">
-                <div className="flex justify-start items-center gap-0 w-[70%] pe-5">
-                  <p className="font-[400] text-[20px] leading-[30px] w-[50%]">
+              <div className="w-full lg:w-[80%] flex justify-center lg:justify-start items-center">
+                <div className="flex justify-start items-center gap-3 lg:gap-0 w-fit lg:w-[70%] pe-5">
+                  <p className="font-[400] text-[20px] leading-[30px] w-full lg:w-[50%]">
                     Country:{" "}
                   </p>
-                  <p className="font-[400] text-[20px] leading-[30px] w-[50%]">
+                  <p className="font-[400] text-[20px] leading-[30px] w-full lg:w-[50%]">
                     Atlanta
                   </p>
                 </div>
