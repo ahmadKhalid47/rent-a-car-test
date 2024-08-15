ef4a83b0b64b666dd9ef0485353e91dd30c1b601
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-08-15 15:28:32 +0500

    scdcfsd

--- app/Components/VehicleForms/Insurances.tsx
+++ app/Components/VehicleForms/Insurances.tsx
@@ -1,11 +1,43 @@
 "use client";
-import { TypeInput } from "../InputComponents/TypeInput";
+import { TypeInput, TempTypeInput } from "../InputComponents/TypeInput";
+import { useSelector } from "react-redux";
+import { RootState } from "@/app/store";
+import {
+  setinsuranceNo,
+  setinsuranceProvider,
+  setinsuranceExpiry,
+} from "@/app/store/Vehicle";
 
 export default function Insurances() {
+  let vehicle = useSelector((state: RootState) => state.Vehicle);
+  console.log(vehicle);
+
   return (
     <div className="w-full h-fit  ">
       <div className="flex flex-wrap justify-start items-start gap-x-[4%] gap-y-5 w-full h-fit bg-white mt-5 rounded-[10px] border-2 border-grey px-1 xs:px-3 md:px-11 py-8">
-        <TypeInput
+        <TempTypeInput
+          setState={setinsuranceNo}
+          label={"Insurance Policy No"}
+          value={vehicle.insuranceNo}
+          required={false}
+          type={"text"}
+        />
+        <TempTypeInput
+          setState={setinsuranceProvider}
+          label={"Insurance Provider"}
+          value={vehicle.insuranceProvider}
+          required={false}
+          type={"text"}
+        />
+        <TempTypeInput
+          setState={setinsuranceExpiry}
+          label={"Insurance Expiry Date"}
+          value={vehicle.insuranceExpiry}
+          required={false}
+          type={"text"}
+        />
+
+        {/* <TypeInput
           label={"Insurance Policy No"}
           value={""}
           required={false}
@@ -22,7 +54,7 @@ export default function Insurances() {
           value={""}
           required={false}
           type={"date"}
-        />
+        /> */}
       </div>
     </div>
   );
