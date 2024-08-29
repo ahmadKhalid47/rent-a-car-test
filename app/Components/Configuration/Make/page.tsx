3f0938f06d3c93667c9f3a09c5eae97ca431e968
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-08-29 18:37:53 +0500

    fvdsv

--- app/Components/Configuration/Make/page.tsx
+++ app/Components/Configuration/Make/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { RootState } from "@/app/store";
+import { FaAsterisk, FaTimes } from "react-icons/fa";
 import { useSelector } from "react-redux";
 import { useMediaQuery } from "react-responsive";
 import { useEffect, useState } from "react";
@@ -13,6 +14,11 @@ import configImg3 from "@/public/configImg (4).svg";
 import configImg4 from "@/public/configImg (5).svg";
 import configImg5 from "@/public/configImg (1).svg";
 import ListView from "./ListView";
+import {
+  TempTypeInput,
+  TempTypeInputWidth,
+} from "../../InputComponents/TypeInput";
+import { setvehicleIdR } from "@/app/store/VehicleUpdate";
 
 export default function Vehicles() {
   let global = useSelector((state: RootState) => state.Global);
@@ -21,6 +27,7 @@ export default function Vehicles() {
   const [showError, setShowError] = useState(null);
   const [vehiclesData, setVehiclesData] = useState<any[]>([]);
   const isMobile = useMediaQuery({ query: "(max-width: 1280px)" });
+  const [popup, setPopup] = useState(false);
 
   useEffect(() => {
     if (isMobile) {
@@ -29,6 +36,9 @@ export default function Vehicles() {
       dispatch(setSidebarShowR(true));
     }
   }, [isMobile]);
+  const handleClick = () => {
+    setPopup(true);
+  };
 
   useEffect(() => {
     async function getData() {
@@ -52,6 +62,10 @@ export default function Vehicles() {
     getData();
   }, [global.vehicleDataReloader]);
 
+  function save() {
+    setPopup(false);
+  }
+
   return (
     <div
       className={`${
@@ -70,8 +84,10 @@ export default function Vehicles() {
           </h3>
           <div className="flex justify-start md:justify-end gap-3 items-end w-[100%] md:w-[50%]">
             <button
-              className="w-fit px-3 md:px-6 py-2 md:py-0 h-fit md:h-[44px] rounded-[10px] bg-main-blue text-white  font-[500] text-[12px] md:text-[18px] leading-[21px] text-center"
-              onClick={() => {}}
+              className="w-fit px-3 md:px-6 py-2 md:py-0 h-fit md:h-[44px] rounded-[10px] bg-main-blue text-white  font-[600] text-[12px] md:text-[18px] leading-[21px] text-center"
+              onClick={() => {
+                handleClick();
+              }}
             >
               Add New
             </button>
@@ -79,6 +95,58 @@ export default function Vehicles() {
         </div>
         <div className="w-full h-fit">
           <ListView data={vehiclesData} />
+
+          {popup ? (
+            <div className="w-full h-full bg-[rgba(255,255,255,0.9)] rounded-[10px] absolute top-0 left-0 flex justify-center item-start sm:items-center z-[10]">
+              <div className="w-[90%] sm:w-[500px] h-fit border-[1px] border-grey rounded-[10px] mt-0 flex flex-wrap justify-between items-start gap-x-[4%] gap-y-5 bg-white shadow z-[15]  py-3 xs:py-5 md:py-14 px-1 xs:px-3 md:px-10 relative">
+                <div
+                  className={`w-[100%] h-fit bg-red-30 flex flex-col justify-start items-start gap-1`}
+                >
+                  <label className="flex justify-start gap-1 items-start font-[400] text-[14px] leading-[17px]">
+                    {"Add New"}
+                    <FaAsterisk className="text-[6px] text-red-600" />
+                  </label>
+                  <div className="w-full h-fit flex justify-between items-center relative overflow-hidden">
+                    <input
+                      required={true}
+                      type={"text"}
+                      className="pe-10 font-[400] text-[16px] leading-[19px] ps-2 w-[100%] h-[43px] flex justify-between items-center input-color rounded-xl border-2 border-grey truncate"
+                      placeholder={`Enter Text Here`}
+                      onChange={(e) => {
+                        // dispatch(setState(e.target.value));
+                      }}
+                      // value={value}
+                    />
+                  </div>
+                </div>
+
+                <div
+                  className={`w-full flex justify-end gap-4 items-center pt-4`}
+                >
+                  <button
+                    className="px-2 md:px-0 w-fit py-2 md:py-0 h-fit md:h-[44px] rounded-[10px] input-color  text-gray-500 font-[400] text-[12px] md:text-[18px] leading-[21px] absolute top-2 right-"
+                    onClick={() => {
+                      setPopup(false);
+                    }}
+                  >
+                    <FaTimes/>
+                  </button>
+                  <button
+                    className="w-fit px-8 py-2 md:py-0 h-fit md:h-[44px] rounded-[10px] bg-main-blue text-white  font-[500] text-[12px] xs:text-[14px] md:text-[18px] leading-[21px] text-center"
+                    onClick={() => save()}
+                  >
+                    Save and Close
+                  </button>
+                  <button
+                    className="w-fit px-8 py-2 md:py-0 h-fit md:h-[44px] rounded-[10px] bg-main-blue text-white  font-[500] text-[12px] xs:text-[14px] md:text-[18px] leading-[21px] text-center"
+                    onClick={() => save()}
+                  >
+                    Save and New
+                  </button>
+                </div>
+              </div>
+            </div>
+          ) : null}
         </div>
       </div>
     </div>
