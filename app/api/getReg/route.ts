b3bbfb91046d276674825086ad97aff7b0d7a06a
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-08-29 14:19:12 +0500

    getRegsdv

--- app/api/getReg/route.ts
+++ app/api/getReg/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
   try {
-    await connectDb();
+    connectDb();
     const data = await RegistrationModel.find();
     const data2 = await VehicleModel.find();
     return NextResponse.json({
