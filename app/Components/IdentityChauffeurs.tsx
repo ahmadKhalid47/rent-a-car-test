89c32be35951cf09e04aa2f52c025af2a5ffd093
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-09-05 09:31:07 +0500

    fbvsbdj

--- app/Components/IdentityChauffeurs.tsx
+++ app/Components/IdentityChauffeurs.tsx
@@ -109,7 +109,7 @@ export default function Generalchauffeur() {
             />
           </div>
           <span
-            className={`cursor-pointer font-[400] text-[30px] p-1 leading-[12px] text-red-500 absolute top-28 right-28 ${
+            className={`cursor-pointer font-[400] text-[30px] p-1 leading-[12px] text-red-500 absolute top-[13%] right-[13%] ${
               zoomed ? "right-3" : ""
             } w-fit shadow bg-white rounded-full`}
             onClick={() => setImagePopup(false)}
