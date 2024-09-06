a9dc2005b447bbc7888f3a72108e89af73e1c6e4
MuhammadAmirSaeed <amirsaeed.gcuf@gmail.com>
2024-09-06 15:15:51 +0500

    Refactor database connection code and add error handling

---
+++ app/Components/ReservationDetails.tsx
@@ -0,0 +1,94 @@
+import React from 'react'
+
+const ReservationDetails = () => {
+  return (
+    <div className='w-full flex justify-between items-start gap-[30%] px-[70px] py-[20px]'>
+<div className=' w-full'>
+        <div className=' w-full flex justify-between '>
+            <div className='font-[400] text-[18px]'>Pick Up Address:</div>
+            <div className='font-[400] text-[18px]'>
+            ABC 1234g
+            </div>
+
+        </div>
+          <div className='border border-t mt-2 mb-2'></div>
+        <div className=' w-full flex justify-between '>
+            <div className='font-[400] text-[18px]'>Drop Off Address:</div>
+            <div className='font-[400] text-[18]'>
+            ABC 1234g
+            </div>
+     
+        </div>
+        <div className='border border-t mt-2 mb-2'></div>
+        <div className=' w-full flex justify-between '>
+            <div className='font-[400] text-[18px]'>Reservation Date:</div>
+            <div className='font-[400] text-[18px]'>
+            15-01-2024
+            </div>
+        </div>
+          <div className='border border-t mt-2 mb-2'></div>
+          <div className=' w-full flex justify-between '>
+            <div className='font-[400] text-[18px]'>Security Deposit:</div>
+            <div className='font-[400] text-[18px]'>
+            $50
+            </div>
+
+        </div>
+        <div className='border border-t mt-2 mb-2'></div>
+          <div className=' w-full flex justify-between '>
+            <div className='font-[400] text-[18px]'>Current Fuel Status:</div>
+            <div className='font-[400] text-[18px]'>
+            50%
+            </div>
+
+        </div>
+        </div>
+      <div className='w-full'>
+
+      <div className=' w-full flex justify-between '>
+            <div className='font-[400] text-[18px]'>Date & Time:</div>
+            <div className='flex gap-2'>
+            <div className='font-[400] text-[18px]'>
+            15-01-2024
+            </div>
+            <div className='font-[400] text-[18px]'>11:30 AM</div>
+            </div>
+
+        </div>
+          <div className='border border-t mt-2 mb-2'></div>
+          
+      <div className=' w-full flex justify-between '>
+            <div className='font-[400] text-[18px]'>Date & Time:</div>
+            <div className='flex gap-2'>
+            <div className='font-[400] text-[18px]'>
+            15-01-2024
+            </div>
+            <div className='font-[400] text-[18px]'>11:30 AM</div>
+            </div>
+
+        </div>
+        <div className='border border-t mt-2 mb-2'></div>
+          <div className=' w-full flex justify-between '>
+            <div className='font-[400] text-[18px]'>Amount:</div>
+            <div className='font-[400] text-[18px]'>
+            $100
+            </div>
+
+        </div>
+        <div className='border border-t mt-2 mb-2'></div>
+          <div className=' w-full flex justify-between '>
+            <div className='font-[400] text-[18px]'>Current Odometer (KMPH):</div>
+            <div className='font-[400] text-[18px]'>
+            67342
+            </div>
+
+        </div>
+       
+
+
+      </div>
+    </div>
+  )
+}
+
+export default ReservationDetails
