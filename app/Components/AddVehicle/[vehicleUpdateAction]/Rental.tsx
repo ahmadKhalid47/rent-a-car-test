be14204ac5302f3284d017aef6d39d8164ad910b
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-09-06 15:45:08 +0500

    nldngl

--- app/Components/AddVehicle/[vehicleUpdateAction]/Rental.tsx
+++ app/Components/AddVehicle/[vehicleUpdateAction]/Rental.tsx
@@ -11,7 +11,7 @@ import {
 
 export default function Rental() {
   let vehicle = useSelector((state: RootState) => state.Vehicle);
-
+ 
   return (
     <div className="w-full h-fit  ">
       <div className="flex flex-wrap justify-start items-start gap-x-[4%] gap-y-5 w-full h-fit bg-white mt-5 rounded-[10px] border-2 border-grey px-1 xs:px-3 md:px-11 py-8">
