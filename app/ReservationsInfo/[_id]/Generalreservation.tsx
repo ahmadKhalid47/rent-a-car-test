c348b6955f21fb1753d6e75a3770443e65788a93
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-09-07 17:49:59 +0500

    complete

--- app/ReservationsInfo/[_id]/Generalreservation.tsx
+++ app/ReservationsInfo/[_id]/Generalreservation.tsx
@@ -51,7 +51,7 @@ export default function Generalreservations() {
               : "---"}
           </p>
         </div>
-        <div className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px]">
+        <div className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px">
           <p className="font-[400] text-[18px] leading-[27px]">
             Current Fuel Status:
           </p>
@@ -89,7 +89,7 @@ export default function Generalreservations() {
           </p>
         </div>
 
-        <div className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px]">
+        <div className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px">
           <p className="font-[400] text-[18px] leading-[27px]">
             Current Odometer (KMPH):
           </p>
