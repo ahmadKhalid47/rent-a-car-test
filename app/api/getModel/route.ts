1fadcdd182c5dfceac4fe914eeb304ff4cae0078
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-09-09 14:59:26 +0500

    cache

--- app/api/getModel/route.ts
+++ app/api/getModel/route.ts
@@ -1,3 +1,23 @@
+// import connectDb from "@/app/models/connectDb";
+// import ModelModel from "@/app/models/Model";
+// import { NextResponse } from "next/server";
+
+// export async function GET(req: Request) {
+//   try {
+//     await connectDb();
+//     const data = await ModelModel.find().sort({ _id: -1 });
+//     return NextResponse.json({
+//       data,
+//     });
+//   } catch (err) {
+//     console.log("err: ", err);
+//     return NextResponse.json({
+//       error: "Can't process your request at the moment",
+//     });
+//   }
+// }
+
+
 import connectDb from "@/app/models/connectDb";
 import ModelModel from "@/app/models/Model";
 import { NextResponse } from "next/server";
@@ -6,9 +26,11 @@ export async function GET(req: Request) {
   try {
     await connectDb();
     const data = await ModelModel.find().sort({ _id: -1 });
-    return NextResponse.json({
+    const response = NextResponse.json({
       data,
     });
+    response.headers.set("Cache-Control", "no-store"); // Ensuring the response is not cached
+    return response;
   } catch (err) {
     console.log("err: ", err);
     return NextResponse.json({
