85a7bf939de0fa7431393e69ff25456ac3fd5b8a
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-09-12 20:39:04 +0500

    dsfsd

--- app/VehicleInfo/[_id]/Damages.tsx
+++ app/VehicleInfo/[_id]/Damages.tsx
@@ -49,148 +49,162 @@ export default function Damages() {
 
   return (
     <div className="w-[100%] h-fit flex justify-between flex-wrap items-start gap-y-[5%] pt-6 pb-8 px-6 border-grey mt-">
-      <div className="w-[23%] h-fit flex flex-col justify-start items-start relative">
-        <img
-          src={
-            vehicleInfo.damages[damageIndex]?.exterior
-              ? exteriorImg
-              : interiorImg
-          }
-          className="w-[250px] h-[300px]"
-        />
-        {vehicleInfo.damages.map((item: any, index: any) => (
-          <>
-            {vehicleInfo.damages[damageIndex]?.exterior ? (
-              item.exterior ? (
-                <div
-                  className={`absolute w-[15px] h-[15px] rounded-full ${
-                    index === damageIndex ? "bg-main-blue" : "bg-grey-of-text"
-                  } text-white text-[8px] flex justify-center items-center font-[600]`}
-                  key={index}
-                  style={{
-                    top: `${item.y}%`,
-                    left: `${item.x}%`,
-                  }}
-                >
-                  {index + 1}
-                </div>
-              ) : null
-            ) : !vehicleInfo.damages[damageIndex]?.exterior ? (
-              !item.exterior ? (
-                <div
-                  className={`absolute w-[15px] h-[15px] rounded-full ${
-                    index === damageIndex ? "bg-main-blue" : "bg-grey-of-text"
-                  } text-white text-[8px] flex justify-center items-center font-[600]`}
-                  key={index}
-                  style={{
-                    top: `${item.y}%`,
-                    left: `${item.x}%`,
-                  }}
-                >
-                  {index + 1}
-                </div>
-              ) : null
-            ) : null}
-          </>
-        ))}
-      </div>
-      <div className="w-[40%] h-fit flex flex-col justify-start items-start  ">
-        <div className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px]">
-          <p className="w-[50px]  font-[600] text-[18px] leading-[27px] text-start">
-            No
-          </p>
-          <p className="w-[60%]  font-[600] text-[18px] leading-[27px] text-start">
-            Damage Type
-          </p>
-          <p className="w-[100px]  font-[600] text-[18px] leading-[27px] text-start">
-            Degree
-          </p>
-        </div>
-        {vehicleInfo.damages.map((item: any, index: number) => (
-          <div
-            className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px] cursor-pointer"
-            onClick={() => {
-              setdamageIndex(index);
-            }}
-          >
-            <p className="w-[50px]  font-[400] text-[18px] leading-[27px] text-start">
-              {index + 1}
-            </p>
-            <p className="w-[60%]  font-[400] text-[18px] leading-[27px] text-start">
-              {item.damageType}
-            </p>
-            <p className="flex justify-between items-center w-[100px]  font-[400] text-[18px] leading-[27px] text-start">
-              {item.degree}
-              <FaEye
-                className={
-                  index === damageIndex ? "text-main-blue" : "text-grey"
-                }
-              />
-            </p>
+      {vehicleInfo.damages.length > 0 ? (
+        <>
+          <div className="w-[23%] h-fit flex flex-col justify-start items-start relative">
+            <img
+              src={
+                vehicleInfo.damages[damageIndex]?.exterior
+                  ? exteriorImg
+                  : interiorImg
+              }
+              className="w-[250px] h-[300px]"
+            />
+            {vehicleInfo.damages.map((item: any, index: any) => (
+              <>
+                {vehicleInfo.damages[damageIndex]?.exterior ? (
+                  item.exterior ? (
+                    <div
+                      className={`absolute w-[15px] h-[15px] rounded-full ${
+                        index === damageIndex
+                          ? "bg-main-blue"
+                          : "bg-grey-of-text"
+                      } text-white text-[8px] flex justify-center items-center font-[600]`}
+                      key={index}
+                      style={{
+                        top: `${item.y}%`,
+                        left: `${item.x}%`,
+                      }}
+                    >
+                      {index + 1}
+                    </div>
+                  ) : null
+                ) : !vehicleInfo.damages[damageIndex]?.exterior ? (
+                  !item.exterior ? (
+                    <div
+                      className={`absolute w-[15px] h-[15px] rounded-full ${
+                        index === damageIndex
+                          ? "bg-main-blue"
+                          : "bg-grey-of-text"
+                      } text-white text-[8px] flex justify-center items-center font-[600]`}
+                      key={index}
+                      style={{
+                        top: `${item.y}%`,
+                        left: `${item.x}%`,
+                      }}
+                    >
+                      {index + 1}
+                    </div>
+                  ) : null
+                ) : null}
+              </>
+            ))}
           </div>
-        ))}
-      </div>
-      <div className="w-[250px] h-[100%] flex flex-col justify-start items-start">
-        {imagePopup ? (
-          <div
-            className="w-[100%] h-[100%] flex justify-center items-center absolute top-0 left-0 bg-[rgba(0,0,0,0.2)]"
-            onClick={() => {
-              setImagePopup(false);
-            }}
-          >
-            <div className="w-[700px] h-[700px] relative overflow-auto scroll border-2 border-black">
+          <div className="w-[40%] h-fit flex flex-col justify-start items-start  ">
+            <div className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px]">
+              <p className="w-[50px]  font-[600] text-[18px] leading-[27px] text-start">
+                No
+              </p>
+              <p className="w-[60%]  font-[600] text-[18px] leading-[27px] text-start">
+                Damage Type
+              </p>
+              <p className="w-[100px]  font-[600] text-[18px] leading-[27px] text-start">
+                Degree
+              </p>
+            </div>
+            {vehicleInfo.damages.map((item: any, index: number) => (
+              <div
+                className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px] cursor-pointer"
+                onClick={() => {
+                  setdamageIndex(index);
+                }}
+              >
+                <p className="w-[50px]  font-[400] text-[18px] leading-[27px] text-start">
+                  {index + 1}
+                </p>
+                <p className="w-[60%]  font-[400] text-[18px] leading-[27px] text-start">
+                  {item.damageType}
+                </p>
+                <p className="flex justify-between items-center w-[100px]  font-[400] text-[18px] leading-[27px] text-start">
+                  {item.degree}
+                  <FaEye
+                    className={
+                      index === damageIndex ? "text-main-blue" : "text-grey"
+                    }
+                  />
+                </p>
+              </div>
+            ))}
+          </div>
+          <div className="w-[250px] h-[100%] flex flex-col justify-start items-start">
+            {imagePopup ? (
+              <div
+                className="w-[100%] h-[100%] flex justify-center items-center absolute top-0 left-0 bg-[rgba(0,0,0,0.2)]"
+                onClick={() => {
+                  setImagePopup(false);
+                }}
+              >
+                <div className="w-[700px] h-[700px] relative overflow-auto scroll border-2 border-black">
+                  <img
+                    src={vehicleInfo.damages[damageIndex]?.files[imageIndex]}
+                    className={"w-[100%] h-[100%]"}
+                    style={{
+                      transform: `${zoomed ? "scale(1.4)" : "scale(1)"}`,
+                      cursor: `${zoomed ? "zoom-out" : "zoom-in"}`,
+                    }}
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      setZoomed(!zoomed);
+                    }}
+                  />
+                </div>
+                <span
+                  className={`cursor-pointer font-[400] text-[30px] p-1 leading-[12px] text-red-500 absolute top-36 right-36 ${
+                    zoomed ? "right-3" : ""
+                  } w-fit shadow bg-white rounded-full`}
+                  onClick={() => setImagePopup(false)}
+                >
+                  <FaTimes />
+                </span>
+              </div>
+            ) : null}
+            <div className="w-[100%] h-[300px] flex justify-center items-center mx-auto">
               <img
                 src={vehicleInfo.damages[damageIndex]?.files[imageIndex]}
                 className={"w-[100%] h-[100%]"}
-                style={{
-                  transform: `${zoomed ? "scale(1.4)" : "scale(1)"}`,
-                  cursor: `${zoomed ? "zoom-out" : "zoom-in"}`,
-                }}
-                onClick={(e) => {
-                  e.stopPropagation();
-                  setZoomed(!zoomed);
+                onClick={() => {
+                  setImagePopup(true);
                 }}
               />
             </div>
-            <span
-              className={`cursor-pointer font-[400] text-[30px] p-1 leading-[12px] text-red-500 absolute top-36 right-36 ${
-                zoomed ? "right-3" : ""
-              } w-fit shadow bg-white rounded-full`}
-              onClick={() => setImagePopup(false)}
-            >
-              <FaTimes />
-            </span>
-          </div>
-        ) : null}
-        <div className="w-[100%] h-[300px] flex justify-center items-center mx-auto">
-          <img
-            src={vehicleInfo.damages[damageIndex]?.files[imageIndex]}
-            className={"w-[100%] h-[100%]"}
-            onClick={() => {
-              setImagePopup(true);
-            }}
-          />
-        </div>
 
-        <div className="w-full h-[30px] mx-auto mt-4 text-[20px] flex justify-between items-center">
-          <FaChevronLeft
-            onClick={() =>
-              setImageIndex(imageIndex > 0 ? imageIndex - 1 : imageIndex)
-            }
-            className={`${imageIndex > 0 ? "" : "text-grey"} cursor-pointer`}
-          />
-          <FaChevronRight
-            onClick={() =>
-              setImageIndex(
-                imageIndex < imageLength - 1 ? imageIndex + 1 : imageIndex
-              )
-            }
-            className={`${
-              imageIndex < imageLength - 1 ? "" : "text-grey"
-            } cursor-pointer`}
-          />
-        </div>
-      </div>
+            <div className="w-full h-[30px] mx-auto mt-4 text-[20px] flex justify-between items-center">
+              <FaChevronLeft
+                onClick={() =>
+                  setImageIndex(imageIndex > 0 ? imageIndex - 1 : imageIndex)
+                }
+                className={`${
+                  imageIndex > 0 ? "" : "text-grey"
+                } cursor-pointer`}
+              />
+              <FaChevronRight
+                onClick={() =>
+                  setImageIndex(
+                    imageIndex < imageLength - 1 ? imageIndex + 1 : imageIndex
+                  )
+                }
+                className={`${
+                  imageIndex < imageLength - 1 ? "" : "text-grey"
+                } cursor-pointer`}
+              />
+            </div>
+          </div>
+        </>
+      ) : (
+        <p className="font-[400] text-[18px] leading-[27px]">
+          No Damage Added
+        </p>
+      )}
     </div>
   );
 }
--- app/VehicleInfo/[_id]/General.tsx
+++ app/VehicleInfo/[_id]/General.tsx
@@ -6,23 +6,22 @@ export default function General() {
 
   return (
     <div className="w-[100%] h-fit flex justify-between flex-wrap items-start gap-x-[5%] gap-y-[5%] pt-6 pb-8 px-6 border-grey mt-">
-      <div className="w-[37%] h-fit flex flex-col justify-start items-start bg-red-30 ">
-
+      <div className="w-[43%] h-fit flex flex-col justify-start items-start bg-red-30 ">
         <div className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px]">
           <p className="font-[400] text-[18px] leading-[27px]">Make:</p>
-          <p className="w-[25%] text-start font-[400] text-[18px] leading-[27px]">
+          <p className="w-[40%] break-words text-start font-[400] text-[18px] leading-[27px]">
             {vehicleInfo.make}
           </p>
         </div>
         <div className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px]">
           <p className="font-[400] text-[18px] leading-[27px]">Year:</p>
-          <p className="w-[25%] text-start font-[400] text-[18px] leading-[27px]">
+          <p className="w-[40%] break-words text-start font-[400] text-[18px] leading-[27px]">
             {vehicleInfo.year}
           </p>
         </div>
         <div className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px]">
           <p className="font-[400] text-[18px] leading-[27px]">Color:</p>
-          <div className="w-[25%] text-start font-[400] text-[18px] leading-[27px] flex justify-start gap-3 items-center">
+          <div className="w-[40%] break-words text-start font-[400] text-[18px] leading-[27px] flex justify-start gap-3 items-center">
             {vehicleInfo.color}
             <div
               className="w-[32px] h-[18px] rounded-[5px]"
@@ -34,49 +33,49 @@ export default function General() {
         </div>
         <div className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px]">
           <p className="font-[400] text-[18px] leading-[27px]">Transmission:</p>
-          <p className="w-[25%] text-start font-[400] text-[18px] leading-[27px]">
+          <p className="w-[40%] break-words text-start font-[400] text-[18px] leading-[27px]">
             {vehicleInfo.transmission}
           </p>
         </div>
 
         <div className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px]">
           <p className="font-[400] text-[18px] leading-[27px]">Passengers:</p>
-          <p className="w-[25%] text-start font-[400] text-[18px] leading-[27px]">
+          <p className="w-[40%] break-words text-start font-[400] text-[18px] leading-[27px]">
             {vehicleInfo.passengers}
           </p>
         </div>
 
         <div className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px">
           <p className="font-[400] text-[18px] leading-[27px]">City:</p>
-          <p className="w-[25%] text-start font-[400] text-[18px] leading-[27px]">
+          <p className="w-[40%] break-words text-start font-[400] text-[18px] leading-[27px]">
             {vehicleInfo.city}
           </p>
         </div>
       </div>
-      <div className="w-[37%] h-fit flex flex-col justify-start items-start bg-red-30 ">
+      <div className="w-[43%] h-fit flex flex-col justify-start items-start bg-red-30 ">
         <div className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px]">
           <p className="font-[400] text-[18px] leading-[27px]">
             Registration No:
           </p>
-          <p className="w-[25%] text-start font-[400] text-[18px] leading-[27px]">
+          <p className="w-[40%] break-words text-start font-[400] text-[18px] leading-[27px]">
             {vehicleInfo.registration}
           </p>
         </div>
         <div className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px]">
           <p className="font-[400] text-[18px] leading-[27px]">Model:</p>
-          <p className="w-[25%] text-start font-[400] text-[18px] leading-[27px]">
+          <p className="w-[40%] break-words text-start font-[400] text-[18px] leading-[27px]">
             {vehicleInfo.model}
           </p>
         </div>
         <div className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px]">
           <p className="font-[400] text-[18px] leading-[27px]">Type:</p>
-          <p className="w-[25%] text-start font-[400] text-[18px] leading-[27px]">
+          <p className="w-[40%] break-words text-start font-[400] text-[18px] leading-[27px]">
             {vehicleInfo.type}
           </p>
         </div>
         <div className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px]">
           <p className="font-[400] text-[18px] leading-[27px]">Fuel Type:</p>
-          <p className="w-[25%] text-start font-[400] text-[18px] leading-[27px]">
+          <p className="w-[40%] break-words text-start font-[400] text-[18px] leading-[27px]">
             {vehicleInfo.fuelType}
           </p>
         </div>
@@ -84,14 +83,14 @@ export default function General() {
           <p className="font-[400] text-[18px] leading-[27px]">
             Odometer (KMPH):
           </p>
-          <p className="w-[25%] text-start font-[400] text-[18px] leading-[27px]">
+          <p className="w-[40%] break-words text-start font-[400] text-[18px] leading-[27px]">
             {vehicleInfo.odometer}
           </p>
         </div>
 
         <div className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px">
           <p className="font-[400] text-[18px] leading-[27px]">Country:</p>
-          <p className="w-[25%] text-start font-[400] text-[18px] leading-[27px]">
+          <p className="w-[40%] break-words text-start font-[400] text-[18px] leading-[27px]">
             {vehicleInfo.country}
           </p>
         </div>
