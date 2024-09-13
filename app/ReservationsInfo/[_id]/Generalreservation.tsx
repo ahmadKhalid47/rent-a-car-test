6bde9d0672a1fca7befa76cf9ae4a1537584fb46
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-09-13 10:09:24 +0500

    jnjkj

--- app/ReservationsInfo/[_id]/Generalreservation.tsx
+++ app/ReservationsInfo/[_id]/Generalreservation.tsx
@@ -9,22 +9,22 @@ export default function Generalreservations() {
 
   return (
     <div className="w-[100%] h-fit flex justify-between flex-wrap items-start gap-x-[5%] gap-y-[5%] pt-6 pb-8 px-6 border-grey mt-">
-      <div className="w-[40%] h-fit flex flex-col justify-start items-start bg-red-30 ">
+      <div className="w-[43%] h-fit flex flex-col justify-start items-start bg-red-30 ">
         <div className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px]">
           <p className="font-[400] text-[18px] leading-[27px]">
             Pick Up Address:
           </p>
-          <p className="w-[30%] text-start font-[400] text-[18px] leading-[27px] truncate">
+          <p className="w-[50%] text-start break-words font-[400] text-[18px] leading-[27px]">
             {reservationInfo?.PickUpAddress
               ? reservationInfo?.PickUpAddress
               : "---"}
           </p>
         </div>
-        <div className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px] truncate">
+        <div className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px]">
           <p className="font-[400] text-[18px] leading-[27px]">
             Drop Off Address:
           </p>
-          <p className="w-[30%] text-start font-[400] text-[18px] leading-[27px]">
+          <p className="w-[50%] text-start break-words font-[400] text-[18px] leading-[27px]">
             {reservationInfo?.dropOffAddress
               ? reservationInfo?.dropOffAddress
               : "---"}
@@ -34,46 +34,39 @@ export default function Generalreservations() {
           <p className="font-[400] text-[18px] leading-[27px]">
             Reservation Date:
           </p>
-          <div className="w-[30%] text-start font-[400] text-[18px] leading-[27px] flex justify-start gap-3 items-center">
+          <div className="w-[50%] text-start break-words font-[400] text-[18px] leading-[27px] flex justify-start gap-3 items-center">
             {reservationInfo?.reservationDate
               ? reservationInfo?.reservationDate
               : "---"}
           </div>
         </div>
-        <div className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px]">
+        <div className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px">
           <p className="font-[400] text-[18px] leading-[27px]">
             Security Deposit:
           </p>
-          <p className="w-[30%] text-start font-[400] text-[18px] leading-[27px]">
+          <p className="w-[50%] text-start break-words font-[400] text-[18px] leading-[27px]">
             $
             {reservationInfo?.securityDeposit
               ? reservationInfo?.securityDeposit
               : "---"}
           </p>
         </div>
-        <div className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px">
-          <p className="font-[400] text-[18px] leading-[27px]">
-            Current Fuel Status:
-          </p>
-          <p className="w-[30%] text-start font-[400] text-[18px] leading-[27px]">
-            {reservationInfo?.fuelStatus ? reservationInfo?.fuelStatus : "---"}%
-          </p>
-        </div>
       </div>
-      <div className="w-[40%] h-fit flex flex-col justify-start items-start bg-red-30 ">
+      <div className="w-[43%] h-fit flex flex-col justify-start items-start bg-red-30 ">
         <div className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px]">
           <p className="font-[400] text-[18px] leading-[27px]">Date & Time: </p>
-          <p className="w-[40%] text-start font-[400] text-[18px] leading-[27px]">
+          <p className="w-[40%] text-start break-words font-[400] text-[18px] leading-[27px]">
             {reservationInfo?.PickUpDate
               ? formatDate(reservationInfo?.PickUpDate)
               : "---"}{" "}
-            {reservationInfo?.PickUpTime ? formatTime(reservationInfo?.PickUpTime) : "---"}
+            {reservationInfo?.PickUpTime
+              ? formatTime(reservationInfo?.PickUpTime)
+              : "---"}
           </p>
         </div>
-
         <div className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px]">
           <p className="font-[400] text-[18px] leading-[27px]">Date & Time: </p>
-          <p className="w-[40%] text-start font-[400] text-[18px] leading-[27px]">
+          <p className="w-[40%] text-start break-words font-[400] text-[18px] leading-[27px]">
             {reservationInfo?.dropOffDate
               ? formatDate(reservationInfo?.dropOffDate)
               : "---"}{" "}
@@ -84,17 +77,24 @@ export default function Generalreservations() {
         </div>
         <div className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px]">
           <p className="font-[400] text-[18px] leading-[27px]">Amount:</p>
-          <p className="w-[40%] text-start font-[400] text-[18px] leading-[27px]">
+          <p className="w-[40%] text-start break-words font-[400] text-[18px] leading-[27px]">
             ${reservationInfo?.amount ? reservationInfo?.amount : "---"}
           </p>
         </div>
-
-        <div className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px">
+        <div className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px]">
           <p className="font-[400] text-[18px] leading-[27px]">
             Current Odometer (KMPH):
           </p>
-          <p className="w-[40%] text-start font-[400] text-[18px] leading-[27px]">
+          <p className="w-[40%] text-start break-words font-[400] text-[18px] leading-[27px]">
             {reservationInfo?.odometer ? reservationInfo?.odometer : "---"}
+          </p>
+        </div>
+        <div className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px">
+          <p className="font-[400] text-[18px] leading-[27px]">
+            Current Fuel Status:
+          </p>
+          <p className="w-[40%] text-start break-words font-[400] text-[18px] leading-[27px]">
+            {reservationInfo?.fuelStatus ? reservationInfo?.fuelStatus : "---"}%
           </p>
         </div>
       </div>
