9f5f2513e245b0cda6c8fa49828e1fc72d636585
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-10-02 13:37:52 +0500

    vdgb

--- app/Settings/MyProfile/UserProfile.tsx
+++ app/Settings/MyProfile/UserProfile.tsx
@@ -119,12 +119,12 @@ export default function UserProfile() {
         <div className="w-[100%]  flex justify-start items-end">
           <h3 className="font-[600] text-[16px] xs:text-[18px] md:text-[25px] leading-5 md:leading-[38px] dark:text-white text-black w-[100%] md:w-[50%]">
             My Profile
-            <span className="text-grey font-[400] text-[12px] xs:text-[14px] md:text-[18px] leading-5 md:leading-[21px]">
+            <p className="text-grey font-[400] text-[12px] xs:text-[14px] md:text-[18px] leading-5 md:leading-[21px]">
               <Link href={"/Settings"} className="hover:underline">
                 Settings
               </Link>{" "}
               / My Profile
-            </span>
+            </p>{" "}
           </h3>
         </div>
         <div className="w-full h-fit dark:bg-dark2 bg-light-grey rounded-xl border-2 border-grey py-5 md:py-6 px-1 xs:px-3 md:px-6 flex flex-col justify-start items-start relative mt-5">
@@ -196,19 +196,6 @@ export default function UserProfile() {
                 <TempTypeInfoShow label={"Expiry Date"} value={"dd-mm-yyyy"} />
                 <TempTypeInfoShow label={"Phone"} value={myProfile.phone} />
                 <TempTypeInfoShow label={"City"} value={myProfile.city} />
-              </div>
-
-              <div
-                className={`w-full h-fit md:h-[100px] pt-6 flex flex-wrap gap-y-2 ${"justify-end"} items-center gap-4`}
-              >
-                <button
-                  onClick={() => {
-                    router.push("/Settings");
-                  }}
-                  className="px-2 md:px-0 w-fit md:w-[140px] py-2 md:py-0 h-fit md:h-[44px] rounded-[10px] dark:bg-dark1 input-color border-2 border-grey text-main-blue  font-[500] text-[12px] md:text-[18px] leading-[21px] text-center"
-                >
-                  Back
-                </button>
               </div>
             </div>
           </div>
