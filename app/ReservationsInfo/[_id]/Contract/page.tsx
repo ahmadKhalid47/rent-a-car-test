d46d071365ffd9aabdbd7ba58c476d0388bb07cb
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-10-04 17:56:52 +0500

    fdsfs

--- app/ReservationsInfo/[_id]/Contract/page.tsx
+++ app/ReservationsInfo/[_id]/Contract/page.tsx
@@ -71,7 +71,7 @@ export default function reservationInfoMainPage() {
           global.sidebarShow ? "nav-width" : "nav-closed-width"
         } h-fit absolute right-0 flex flex-col justify-start items-start gap-[20px]   pe-[10px] md:pe-[50px] ps-[10px] md:ps-[20px]  pb-14`}
       >
-        <div className="w-[100%] gap-y-3 flex flex-wrap justify-between md:justify-start items-end">
+        <div className="w-[100%] gap-y-3 sm:gap-y-0 flex flex-wrap justify-between md:justify-start items-end">
           <h3 className="font-[600] text-[16px] xs:text-[18px] md:text-[25px] leading-5 md:leading-[38px] dark:text-white text-black w-[100%] md:w-[50%]">
             Contract
             <p className="text-grey font-[400] text-[12px] xs:text-[14px] md:text-[18px] leading-5 md:leading-[21px]">
--- app/Settings/page.tsx
+++ app/Settings/page.tsx
@@ -116,7 +116,7 @@ export default function Vehicles() {
       <div
         className={`w-full h-fit flex flex-col justify-start items-start gap-[0px] md:gap-[20px] pe-[10px] md:pe-[50px] ps-[10px] md:ps-[40px] pb-10`}
       >
-        <div className="w-[100%] gap-y-3 flex flex-wrap justify-between md:justify-start items-end">
+        <div className="w-[100%] gap-y-3 sm:gap-y-0 flex flex-wrap justify-between md:justify-start items-end">
           <h3 className="font-[600] text-[16px] xs:text-[18px] md:text-[25px] leading-5 md:leading-[38px] dark:text-white text-black w-[100%] md:w-[50%]">
             Settings
           </h3>
