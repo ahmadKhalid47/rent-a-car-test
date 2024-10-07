fc13c8ac3f6d67ece9119481925d6b5a13b6011b
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-10-07 18:46:21 +0500

    gdfgbdf

--- app/VehicleInfo/[_id]/Damages.tsx
+++ app/VehicleInfo/[_id]/Damages.tsx
@@ -53,17 +53,17 @@ export default function Damages() {
   )?.interior;
 
   return (
-    <div className="w-[100%] h-fit flex justify-between flex-wrap items-start gap-y-[5%] pt-6 pb-8 px-6 border-grey mt-">
+    <div className="w-full h-full py-4 px-5 flex justify-start items-start gap-1 overflow-auto">
       {vehicleInfo.damages.length > 0 ? (
         <>
-          <div className="w-[250px] h-[300px] flex flex-col justify-start items-start relative bg-green-300">
+          <div className="w-[70px h-[120px] flex flex-col justify-start items-start relative">
             <img
               src={
                 vehicleInfo.damages[damageIndex]?.exterior
                   ? exteriorImg
                   : interiorImg
               }
-              className="w-[250px] h-[300px] bg-white"
+              className="w-[70px h-[120px] bg-white"
             />
             {vehicleInfo.damages.map((item: any, index: any) => (
               <>
@@ -105,49 +105,72 @@ export default function Damages() {
               </>
             ))}
           </div>
-          <div className="w-[40%] h-fit flex flex-col justify-start items-start  ">
-            <div className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px]">
-              <span className="py-2 w-[50px]  font-[600] text-[18px] leading-[27px] text-start">
+          <div className="w-[80%]  h-fit flex flex-col justify-start items-start">
+            <div className="w-full h-[25px] flex justify-between items-center border-b-[2px">
+              <span className="w-[30px] font-[600] text-[12px] xs:text-[14px] md:text-[13px] leading-[27px]">
+                Image
+              </span>
+              <span className="w-[20%] md:w-[25px]  font-[600] text-[12px] xs:text-[14px] md:text-[13px] leading-[27px]">
                 No
               </span>
-              <span className="py-2 w-[40%]  font-[600] text-[18px] leading-[27px] text-start">
-                Damage Type
+              <span className="w-[20%] font-[600] text-[12px] xs:text-[14px] md:text-[13px] leading-[27px]">
+                Type
               </span>
-              <span className="py-2 w-[150px]  font-[600] text-[18px] leading-[27px] text-start">
+              <span className="w-[15%] font-[600] text-[12px] xs:text-[14px] md:text-[13px] leading-[27px]">
                 Position
               </span>
-              <span className="py-2 w-[100px]  font-[600] text-[18px] leading-[27px] text-start">
+              <span className="w-[20%] md:w-[100px]  font-[600] text-[12px] xs:text-[14px] md:text-[13px] leading-[27px]">
                 Degree
               </span>
-            </div>
+              <span className="text-transparent font-[400] text-[12px] xs:text-[14px] md:text-[13px] leading-none text-end">
+                <FaEye />
+              </span>
+            </div>{" "}
             {vehicleInfo.damages.map((item: any, index: number) => (
-              <div
-                className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px] cursor-pointer"
-                onClick={() => {
-                  setdamageIndex(index);
-                }}
-              >
-                <span className="py-2 w-[50px]  font-[400] text-[18px] leading-[27px] text-start">
-                  {index + 1}
+              // <div className="w-full h-fit flex justify-between items-start py-[3px] border-b-[2px] cursor-pointer">
+              //   <span className="py-2 w-[50px]  font-[400] text-[14px] leading-[27px] text-start">
+              //     {index + 1}
+              //   </span>
+              //   <span className="py-2 w-[40%]  font-[400] text-[14px] leading-[27px] text-start">
+              //     {item.damageType}
+              //   </span>
+              //   <span className="py-2 w-[150px]  font-[400] text-[14px] leading-[27px] text-start">
+              //     {item.exterior ? "Exterior" : "Interior"}
+              //   </span>
+              //   <span className="py-2 flex justify-between items-center w-[100px]  font-[400] text-[14px] leading-[27px] text-start">
+              //     {item.degree}
+              //     <FaEye
+              //       className={
+              //         index === damageIndex ? "text-main-blue" : "text-grey"
+              //       }
+              //     />
+              //   </span>
+              // </div>
+              <div className="w-full h-[30px] mt-1 flex justify-between items-end border-b-[2px]">
+                <img
+                  className="w-[30px] h-[20px] mb-1 font-[400] text-[12px] xs:text-[14px] md:text-[13px] leading-none rounded-[5px]"
+                  src={item?.files[0]}
+                />
+                <span className="pb-2 w-[20%] md:w-[25px] font-[400] text-[12px] xs:text-[14px] md:text-[13px] leading-none">
+                  {JSON.stringify(index + 1).padStart(2, "0")}{" "}
                 </span>
-                <span className="py-2 w-[40%]  font-[400] text-[18px] leading-[27px] text-start">
-                  {item.damageType}
+                <span className="pb-2 w-[20%] font-[400] text-[12px] xs:text-[14px] md:text-[13px] leading-none">
+                  {item?.damageType}
                 </span>
-                <span className="py-2 w-[150px]  font-[400] text-[18px] leading-[27px] text-start">
-                  {item.exterior ? "Exterior" : "Interior"}
+                <span className="pb-2 w-[15%] font-[400] text-[12px] xs:text-[14px] md:text-[13px] leading-none">
+                  {item?.exterior ? "Exterior" : "Interior"}
                 </span>
-                <span className="py-2 flex justify-between items-center w-[100px]  font-[400] text-[18px] leading-[27px] text-start">
-                  {item.degree}
-                  <FaEye
-                    className={
-                      index === damageIndex ? "text-main-blue" : "text-grey"
-                    }
-                  />
+                <span className="pb-2 w-[20%] md:w-[100px] font-[400] text-[12px] xs:text-[14px] md:text-[13px] leading-none">
+                  {item?.degree}
+                </span>
+                <span className="pb-2 font-[400] text-[12px] xs:text-[14px] md:text-[13px] leading-none text-end hover:scale-[1.3] cursor-pointer">
+                  <FaEye />
                 </span>
               </div>
             ))}
           </div>
-          <div className="w-[250px] h-[100%] flex flex-col justify-start items-start">
+
+          {/* <div className="w-[250px] h-[100%] flex flex-col justify-start items-start">
             {imagePopup ? (
               <div
                 className="w-[100%] h-[100%] flex justify-center items-center absolute top-0 left-0 bg-[rgba(0,0,0,0.2)]"
@@ -189,36 +212,12 @@ export default function Damages() {
                   image404.src
                 }
                 className={"w-[100%] h-[100%]"}
-                onClick={() => {
-                  setImagePopup(true);
-                }}
               />
             </div>
-
-            <div className="w-full h-[30px] mx-auto mt-4 text-[20px] flex justify-between items-center">
-              <FaChevronLeft
-                onClick={() =>
-                  setImageIndex(imageIndex > 0 ? imageIndex - 1 : imageIndex)
-                }
-                className={`${
-                  imageIndex > 0 ? "" : "text-grey"
-                } cursor-pointer`}
-              />
-              <FaChevronRight
-                onClick={() =>
-                  setImageIndex(
-                    imageIndex < imageLength - 1 ? imageIndex + 1 : imageIndex
-                  )
-                }
-                className={`${
-                  imageIndex < imageLength - 1 ? "" : "text-grey"
-                } cursor-pointer`}
-              />
-            </div>
-          </div>
+          </div> */}
         </>
       ) : (
-        <span className="py- font-[400] text-[18px] leading-[27px]">
+        <span className="py- font-[400] text-[14px] leading-[27px]">
           No Damage Added
         </span>
       )}
