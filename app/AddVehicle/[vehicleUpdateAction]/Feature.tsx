56e1b74a89fe683e4961dec0b69c1562b4497960
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-10-11 16:32:05 +0500

    sdvsfds

--- app/AddVehicle/[vehicleUpdateAction]/Feature.tsx
+++ app/AddVehicle/[vehicleUpdateAction]/Feature.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { TypeInput, TempTypeInput } from "../../Components/InputComponents/TypeInput";
+import demyIcon from "@/public/features (1).png";
 import { useSelector } from "react-redux";
 import { RootState } from "@/app/store";
 import { useDispatch } from "react-redux";
@@ -11,10 +11,12 @@ import Link from "next/link";
 export default function Feature() {
   let vehicle = useSelector((state: RootState) => state.Vehicle);
   let Configurations = useSelector((state: RootState) => state.Configurations);
-  console.log();
 
   let featuresDisplayArray: any = Configurations?.Configurations?.feature?.map(
     (item: any) => item.Feature
+  );
+  let iconsDisplayArray: any = Configurations?.Configurations?.feature?.map(
+    (item: any) => item.Icon
   );
   let dispatch = useDispatch();
 
@@ -32,12 +34,13 @@ export default function Feature() {
       return newArray;
     });
   }
+  console.log(iconsDisplayArray);
 
   return (
     <div className="w-full h-fit  ">
       <div className="w-full h-fit">
         <div className="flex flex-wrap justify-start items-start gap-x-[4%] lg:gap-x-[6.66%] gap-y-5 w-full h-fit dark:bg-dark1 bg-white mt-5 rounded-[10px] border-2 border-grey  px-1 xs:px-3 md:px-11 py-8">
-          {featuresDisplayArray?.map((item: any, key: any) => (
+          {featuresDisplayArray?.map((item: any, index: any) => (
             <button
               className={`w-[100%] sm:w-[48%] lg:w-[20%] py-2 md:py-0 h-fit md:h-[44px] rounded-[10px] truncate px-3
                 ${
@@ -45,12 +48,22 @@ export default function Feature() {
                     ? "bg-main-blue text-white font-[500]"
                     : "dark:bg-dark1 input-color border-2 border-grey font-[400]"
                 } 
-                font-[500] text-[12px] xs:text-[14px] md:text-[18px] leading-[21px] text-center`}
-              key={key}
+                font-[500] text-[12px] xs:text-[14px] md:text-[18px] leading-[21px] text-center flex justify-start items-center gap-4`}
+              key={index}
               onClick={() => {
                 handleClick(item);
               }}
             >
+              <img
+                className="w-[20px] h-[20px]"
+                src={
+                  iconsDisplayArray[index]
+                    ? iconsDisplayArray[index]
+                    : demyIcon.src
+                }
+                alt=""
+              />
+
               {item}
             </button>
           ))}
