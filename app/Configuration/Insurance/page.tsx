4efa04a019e50f485d68e011607e11c76be11c10
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-10-18 11:58:53 +0500

    Insurance

--- app/Configuration/Insurance/page.tsx
+++ app/Configuration/Insurance/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import shape from "@/public/ShapeBlack.svg";
 import React from "react";
 import { RootState } from "@/app/store";
 import { FaAsterisk, FaTimes } from "react-icons/fa";
@@ -23,9 +24,16 @@ export default function Vehicles() {
   const [showError, setShowError] = useState(null);
   const [vehiclesData, setVehiclesData] = useState<any[]>([]);
   const isMobile = useMediaQuery({ query: "(max-width: 1280px)" });
-  const [popup, setPopup] = useState(false);
+  const [popup, setOtherPopup] = useState(false);
   const [Insurance, setInsurance] = useState("");
-  
+  const [recurring, setrecurring] = useState("");
+  let [other, setOther] = useState("");
+  let [otherPopUp, setOtherPopUp] = useState(false);
+
+  useEffect(() => {
+    if (recurring === "Other") setOtherPopUp(true);
+  }, [recurring]);
+
   useEffect(() => {
     if (isMobile) {
       dispatch(setSidebarShowR(false));
@@ -34,7 +42,7 @@ export default function Vehicles() {
     }
   }, [isMobile]);
   const handleClick = () => {
-    setPopup(true);
+    setOtherPopup(true);
   };
 
   useEffect(() => {
@@ -66,7 +74,8 @@ export default function Vehicles() {
       return;
     } else if (
       vehiclesData.find(
-        (item) => item.Insurance.toLowerCase() === Insurance.trim().toLowerCase()
+        (item) =>
+          item.Insurance.toLowerCase() === Insurance.trim().toLowerCase()
       )
     ) {
       dispatch(setAlert("This Item Already Exists"));
@@ -80,11 +89,11 @@ export default function Vehicles() {
         Insurance,
         createdBy: myProfile._id,
       });
-      
+
       dispatch(setAlert("Insurance Saved Successfully"));
       dispatch(setVehicleDataReloader(global.vehicleDataReloader + 1));
       if (action === "close") {
-        setPopup(false);
+        setOtherPopup(false);
       }
       setInsurance("");
     } catch (err) {
@@ -93,6 +102,16 @@ export default function Vehicles() {
       setLoading("");
     }
   }
+
+  let options = [
+    "Daily",
+    "Weekly",
+    "Three Weekly",
+    "Monthly",
+    "Six Monthly",
+    "Yearly",
+  ];
+console.log(other);
 
   return (
     <div
@@ -152,6 +171,37 @@ export default function Vehicles() {
                     />
                   </div>
                 </div>
+                <div className="w-[100%] h-fit flex flex-col justify-start items-start gap-1">
+                  <label className="flex justify-start gap-1 items-start font-[600] text-[14px] leading-[17px]">
+                    Select Recurring
+                    <FaAsterisk className="text-[6px]" />
+                  </label>
+                  <div className="w-full h-fit flex justify-between items-center relative overflow-hidde">
+                    <select
+                      className="pe-10 font-[400] text-[16px] leading-[19px] ps-1 w-[100%] h-[43px] flex justify-between items-center dark:bg-dark1 input-color rounded-xl border-2 border-grey"
+                      required={true}
+                      onChange={(e) => {
+                        setrecurring(e.target.value);
+                      }}
+                      value={recurring}
+                    >
+                      <option value={""}>Select</option>
+                      {options?.map((item: any, key: number) => (
+                        <option value={item} key={key}>
+                          {item ? item : "Select"}
+                        </option>
+                      ))}{" "}
+                      {other && <option value={other}>{other}</option>}
+                      <option value={"Other"}>Other</option>
+                    </select>
+                    <div className="w-[30px] h-[35px] dark:bg-dark1 input-color absolute right-1 rounded-xl flex justify-center items-center pointer-events-none">
+                      <img
+                        src={shape.src}
+                        className="w-[10.5px]  dark:filter dark:brightness-[0] dark:invert"
+                      />
+                    </div>
+                  </div>
+                </div>
 
                 <div
                   className={`w-full flex justify-end gap-4 items-center pt-4`}
@@ -159,7 +209,7 @@ export default function Vehicles() {
                   <button
                     className="px-2 md:px-0 w-fit py-2 md:py-0 h-fit md:h-[44px] rounded-[10px] dark:bg-dark1 input-color  text-gray-500 font-[400] text-[12px] md:text-[18px] leading-[21px] absolute top-2 right-"
                     onClick={() => {
-                      setPopup(false);
+                      setOtherPopup(false);
                       setInsurance("");
                     }}
                   >
@@ -183,6 +233,56 @@ export default function Vehicles() {
               </div>
             </div>
           ) : null}
+          {otherPopUp && (
+            <div className="w-full h-full dark:bg-blackOpacity bg-[rgba(255,255,255,0.9)] rounded-[10px] absolute top-[0px] left-0 flex justify-center item-center sm:items-center z-[10]">
+              <div className="w-[90%] sm:w-[500px] h-fit border-[1px] border-grey rounded-[10px] mt-0 flex flex-wrap justify-between items-start gap-x-[4%] gap-y-5 dark:bg-dark1 bg-white shadow z-[15]  py-3 xs:py-5 md:py-14 px-1 xs:px-3 md:px-10 modal-position">
+                <div
+                  className={`w-[100%] h-fit flex flex-col justify-start items-start gap-1`}
+                >
+                  <label className="flex justify-start gap-1 items-start font-[600] text-[14px] leading-[17px]">
+                    {"Add New"}
+                    <FaAsterisk className="text-[6px]" />
+                  </label>
+                  <div className="w-full h-fit flex justify-between items-center relative overflow-hidde">
+                    <input
+                      required={true}
+                      type={"text"}
+                      className="pe-10 font-[400] text-[16px] leading-[19px] ps-2 w-[100%] h-[43px] flex justify-between items-center dark:bg-dark1 input-color rounded-xl border-2 border-grey truncate"
+                      placeholder={`Enter Text Here`}
+                      onChange={(e) => {
+                        setOther(e.target.value);
+                      }}
+                      value={other}
+                    />
+                  </div>
+                </div>
+
+                <div
+                  className={`w-full flex justify-end gap-4 items-center pt-4`}
+                >
+                  <button
+                    className="px-2 md:px-0 w-fit py-2 md:py-0 h-fit md:h-[44px] rounded-[10px] dark:bg-dark1 input-color  text-gray-500 font-[400] text-[12px] md:text-[18px] leading-[21px] absolute top-2 right-"
+                    onClick={() => {
+                      setOther("");
+                      setOtherPopUp(false);
+                    }}
+                  >
+                    <FaTimes />
+                  </button>
+                  <button
+                    className="w-[230px] py-2 md:py-0 h-fit md:h-[44px] rounded-[10px] bg-main-blue text-white  font-[500] text-[12px] xs:text-[14px] md:text-[18px] leading-[21px] text-center"
+                    onClick={() => {
+                      setrecurring(other);
+                      setOtherPopUp(false);
+                      // setOther("");
+                    }}
+                  >
+                    Save
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
