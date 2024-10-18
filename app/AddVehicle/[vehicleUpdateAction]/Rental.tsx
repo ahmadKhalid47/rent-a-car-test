2d2c78259f0916a886d3e62c539c7da7939901b1
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-10-18 15:23:03 +0500

    njknkdvds

--- app/AddVehicle/[vehicleUpdateAction]/Rental.tsx
+++ app/AddVehicle/[vehicleUpdateAction]/Rental.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { TypeInput, TempTypeInput } from "../../Components/InputComponents/TypeInput";
+import { TypeInput, TempTypeInputSign } from "../../Components/InputComponents/TypeInput";
 import { useSelector } from "react-redux";
 import { RootState } from "@/app/store";
 import {
@@ -12,40 +12,103 @@ import {
 
 export default function Rental() {
   let vehicle = useSelector((state: RootState) => state.Vehicle);
+  let global = useSelector((state: RootState) => state.Global);
  
   return (
     <div className="w-full h-fit  ">
       <div className="flex flex-wrap justify-start items-start gap-x-[4%] gap-y-5 w-full h-fit dark:bg-dark1 bg-white mt-5 rounded-[10px] border-2 border-grey px-1 xs:px-3 md:px-11 py-8">
         <span className="flex justify-start gap-1 items-center font-[600] text-[20px] w-full my-1 c">
-          Rental Information
+          Rental Price
         </span>
-        <TempTypeInput
+        <TempTypeInputSign
           setState={setrentHour}
-          label={"Rental Price Per Hour"}
+          label={"Per Hour"}
           value={vehicle.rentHour}
           required={false}
           type={"number"}
+          sign={global.currentCurrency}
         />
-        <TempTypeInput
+        <TempTypeInputSign
           setState={setrentDay}
-          label={"Rental Price Per Day"}
+          label={"Per Day"}
           value={vehicle.rentDay}
           required={false}
           type={"number"}
+          sign={global.currentCurrency}
         />
-        <TempTypeInput
+        <TempTypeInputSign
           setState={setrentWeek}
-          label={"Rental Price Per Week"}
+          label={"Per Week"}
           value={vehicle.rentWeek}
           required={false}
           type={"number"}
+          sign={global.currentCurrency}
         />
-        <TempTypeInput
+        <TempTypeInputSign
           setState={setrentMonth}
-          label={"Rental Price Per Month"}
+          label={"Per Month"}
           value={vehicle.rentMonth}
           required={false}
           type={"number"}
+          sign={global.currentCurrency}
+        />
+      </div>
+      <div className="flex flex-wrap justify-start items-start gap-x-[4%] gap-y-5 w-full h-fit dark:bg-dark1 bg-white mt-5 rounded-[10px] border-2 border-grey px-1 xs:px-3 md:px-11 py-8">
+        <span className="flex justify-start gap-1 items-center font-[600] text-[20px] w-full my-1 c">
+          Mileage Limit
+        </span>
+        <TempTypeInputSign
+          setState={setrentDay}
+          label={"Per Day"}
+          value={vehicle.rentDay}
+          required={false}
+          type={"number"}
+          sign={"KM"}
+        />
+        <TempTypeInputSign
+          setState={setrentWeek}
+          label={"Per Week"}
+          value={vehicle.rentWeek}
+          required={false}
+          type={"number"}
+          sign={"KM"}
+        />
+        <TempTypeInputSign
+          setState={setrentMonth}
+          label={"Per Month"}
+          value={vehicle.rentMonth}
+          required={false}
+          type={"number"}
+          sign={"KM"}
+        />
+        <TempTypeInputSign
+          setState={setrentHour}
+          label={"Excess Mileage Fee"}
+          value={vehicle.rentHour}
+          required={false}
+          type={"number"}
+          sign={global.currentCurrency}
+        />
+      </div>
+      <div className="flex flex-wrap justify-start items-start gap-x-[4%] gap-y-5 w-full h-fit dark:bg-dark1 bg-white mt-5 rounded-[10px] border-2 border-grey px-1 xs:px-3 md:px-11 py-8">
+        <span className="flex justify-start gap-1 items-center font-[600] text-[20px] w-full my-1 c">
+          Late Return Fee
+        </span>
+        <TempTypeInputSign
+          setState={setrentHour}
+          label={"Per Hour"}
+          value={vehicle.rentHour}
+          required={false}
+          type={"number"}
+          sign={global.currentCurrency}
+        />
+        <TempTypeInputSign
+          setState={setrentDay}
+          label={"Per Day"}
+          value={vehicle.rentDay}
+          required={false}
+          type={"number"}
+          sign={global.currentCurrency}
         />
       </div>
     </div>
--- app/Components/InputComponents/TypeInput.tsx
+++ app/Components/InputComponents/TypeInput.tsx
@@ -174,6 +174,53 @@ export const TempTypeInput: React.FC<TempTypeInput> = ({
   );
 };
 
+// Sign
+interface TempTypeInputSign {
+  label: string;
+  setState: any;
+  value: any;
+  required: boolean;
+  type: string;
+  sign: string;
+}
+
+export const TempTypeInputSign: React.FC<TempTypeInputSign> = ({
+  label,
+  setState,
+  value,
+  required,
+  type,
+  sign,
+}) => {
+  let dispatch = useDispatch();
+
+  return (
+    <div className="w-[100%] sm:w-[48%] lg:w-[22%] h-fit flex flex-col justify-start items-start gap-1">
+      <label className="flex justify-start gap-1 items-start font-[400] text-[14px] leading-[17px]">
+        {label}
+        {required && <FaAsterisk className="text-[6px]" />}
+      </label>
+      <div className="w-full h-fit flex justify-between items-center relative overflow-hidde">
+        <input
+          required={required}
+          type={type}
+          autoComplete="new-password"
+          className="pe-10 font-[400] text-[16px] leading-[19px] ps-2 w-[100%] h-[43px] flex justify-between items-center dark:bg-dark1 input-color rounded-xl border-2 border-grey truncate"
+          placeholder={`Enter ${label}`}
+          onChange={(e) => {
+            dispatch(setState(e.target.value));
+          }}
+          value={value}
+        />
+        <div className="absolute right-3 text-[#808080]">{sign}</div>
+      </div>
+    </div>
+  );
+};
+
+
+
+
 interface TempTypeInputLimit {
   label: string;
   setState: any;
