7d1839d1de6551ea78e051f6f70687cac34e0918
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-10-21 11:20:56 +0500

    fvsdfjs

--- app/AddVehicle/[vehicleUpdateAction]/Insurances.tsx
+++ app/AddVehicle/[vehicleUpdateAction]/Insurances.tsx
@@ -102,7 +102,14 @@ export default function Insurances() {
           required={false}
           type={"date"}
         />
-        <TempSelectInputLink
+        <TempTypeInput
+          setState={setinsRecurringPeriod}
+          label={"Recurring Period"}
+          value={vehicle.insRecurringPeriod}
+          required={false}
+          type={"text"}
+        />
+        {/* <TempSelectInputLink
           setState={setinsRecurringPeriod}
           label={"Recurring Period"}
           value={vehicle.insRecurringPeriod}
@@ -111,7 +118,7 @@ export default function Insurances() {
             (item: any) => item.recurring
           )}
           link={"/Configuration/Insurance"}
-        />
+        /> */}
         <TempTypeInput
           setState={setinsRecurringDate}
           label={"Recurring Date"}
