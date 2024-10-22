7df5dd4d8ed73c946e67affce3d14fbd7d8d88fa
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-10-22 11:08:57 +0500

    mklmlk

--- app/Vehicles/ListView.tsx
+++ app/Vehicles/ListView.tsx
@@ -237,7 +237,7 @@ export default function ListView({ data }: dataType) {
         )}
       </div>
       <div className="w-full h-fit overflow-auto rounded-[10px] border-2 border-grey mt-2 relative">
-        <div className="w-[900px] 1200:w-full h-fit flex flex-col justify-start items-start dark:bg-dark2 bg-light-grey overflow-hidden mt-0 leading-[17px]">
+        <div className="w-[1100px] 1400:w-full h-fit flex flex-col justify-start items-start dark:bg-dark2 bg-light-grey overflow-hidden mt-0 leading-[17px]">
           <div className="w-full h-[43px] flex justify-between items-center font-[600] text-[12px] sm:text-[14px] rounded-t-[10px] leading-[17px text-center border-b-2 border-grey">
             <div className="truncate  w-[4%]  flex justify-start ps-3 items-center">
               <div
