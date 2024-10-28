b57a53c9db85b1f6a25a7f2cac46b7787285b294
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-10-28 12:37:38 +0500

    nkj

--- app/Chauffeurs/page.tsx
+++ app/Chauffeurs/page.tsx
@@ -37,7 +37,7 @@ export default function chauffeurs() {
       keyValue: "",
     },
     {
-      key: "postalCode",
+      key: "rentPerDay",
       keyValue: "",
     },
   ]);
@@ -147,7 +147,7 @@ export default function chauffeurs() {
         keyValue: "",
       },
       {
-        key: "postalCode",
+        key: "rentPerDay",
         keyValue: "",
       },
     ]);
@@ -200,30 +200,32 @@ export default function chauffeurs() {
                 <CiSearch />
               </div>
             </div>
-            {/* <div className="w-[144px] h-fit ">
+            <div className="w-[144px] h-fit ">
               <div className="w-full h-fit flex justify-between items-center relative">
                 <select
                   className="truncate pe-3 font-[400] text-[14px] xs:text-[16px] leading-[19px] ps-6 w-[100%] h-[44px] flex justify-between items-center dark:bg-dark1 bg-white rounded-[5px] border-2 border-grey "
                   onChange={(e) => {
                     setAdvanceFilters((prevFilters: any) =>
                       prevFilters.map((filter: any) =>
-                        filter.key === "type"
+                        filter.key === "rentPerDay"
                           ? { ...filter, keyValue: e.target.value }
                           : filter
                       )
                     );
                   }}
                   value={
-                    advanceFilters.find((filter: any) => filter.key === "type")
-                      ?.keyValue || ""
+                    advanceFilters.find(
+                      (filter: any) => filter.key === "rentPerDay"
+                    )?.keyValue || ""
                   }
                 >
-                  <option value="">Type</option>
+                  <option value="">Rent</option>
                   {Array.from(
-                    new Set(chauffeursData.map((item) => item.data.type))
-                  ).map((type) => (
-                    <option key={type} value={type}>
-                      {type}
+                    new Set(chauffeursData.map((item) => item.data.rentPerDay))
+                  ).map((rentPerDay) => (
+                    <option key={rentPerDay} value={rentPerDay}>
+                      {global.currentCurrency}
+                      {rentPerDay}
                     </option>
                   ))}
                 </select>
@@ -237,7 +239,7 @@ export default function chauffeurs() {
                   <CiFilter />
                 </div>
               </div>
-            </div> */}
+            </div>
             <div className="w-[144px] h-fit ">
               <div className="w-full h-fit flex justify-between items-center relative">
                 <select
@@ -276,122 +278,6 @@ export default function chauffeurs() {
           </div>
 
           <div className="w-full flex flex-wrap gap-y-2 1400:flex-nowrap h-fit justify-between items-center">
-            {/* <div className="w-[100%] xs:w-[48%] lg:w-[30%]  h-fit flex flex-col justify-start items-start gap-1">
-              <label className="flex justify-start gap-1 items-start font-[400] text-[14px] leading-[17px]">
-                Gender
-              </label>
-              <div className="w-full h-fit flex justify-between items-center relative">
-                <select
-                  className="pe-10 font-[400] text-[16px] leading-[19px] ps-1 w-[100%] h-[43px] flex justify-between items-center dark:bg-dark1 bg-white rounded-xl border-2 border-grey"
-                  onChange={(e) => {
-                    setAdvanceFilters((prevFilters: any) =>
-                      prevFilters.map((filter: any) =>
-                        filter.key === "gender"
-                          ? { ...filter, keyValue: e.target.value }
-                          : filter
-                      )
-                    );
-                  }}
-                  value={
-                    advanceFilters.find(
-                      (filter: any) => filter.key === "gender"
-                    )?.keyValue || ""
-                  }
-                >
-                  <option value="">Select</option>
-                  {Array.from(
-                    new Set(chauffeursData.map((item) => item.data.gender))
-                  ).map((gender) => (
-                    <option key={gender} value={gender}>
-                      {gender}
-                    </option>
-                  ))}
-                </select>
-                <div className="w-[30px] h-[35px] dark:bg-dark1 bg-white absolute right-1 rounded-xl flex justify-center items-center pointer-events-none">
-                  <img
-                    src={shape.src}
-                    className="w-[10.5px]  dark:filter dark:brightness-[0] dark:invert"
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="w-[100%] xs:w-[48%] lg:w-[30%]  h-fit flex flex-col justify-start items-start gap-1">
-              <label className="flex justify-start gap-1 items-start font-[400] text-[14px] leading-[17px]">
-                Postal/Zip Code
-              </label>
-              <div className="w-full h-fit flex justify-between items-center relative">
-                <select
-                  className="pe-10 font-[400] text-[16px] leading-[19px] ps-1 w-[100%] h-[43px] flex justify-between items-center dark:bg-dark1 bg-white rounded-xl border-2 border-grey"
-                  onChange={(e) => {
-                    setAdvanceFilters((prevFilters: any) =>
-                      prevFilters.map((filter: any) =>
-                        filter.key === "postalCode"
-                          ? { ...filter, keyValue: e.target.value }
-                          : filter
-                      )
-                    );
-                  }}
-                  value={
-                    advanceFilters.find(
-                      (filter: any) => filter.key === "postalCode"
-                    )?.keyValue || ""
-                  }
-                >
-                  <option value="">Select</option>
-                  {Array.from(
-                    new Set(chauffeursData.map((item) => item.data.postalCode))
-                  ).map((postalCode) => (
-                    <option key={postalCode} value={postalCode}>
-                      {postalCode}
-                    </option>
-                  ))}
-                </select>
-                <div className="w-[30px] h-[35px] dark:bg-dark1 bg-white absolute right-1 rounded-xl flex justify-center items-center pointer-events-none">
-                  <img
-                    src={shape.src}
-                    className="w-[10.5px]  dark:filter dark:brightness-[0] dark:invert"
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="w-[100%] xs:w-[48%] lg:w-[30%]  h-fit flex flex-col justify-start items-start gap-1">
-              <label className="flex justify-start gap-1 items-start font-[400] text-[14px] leading-[17px]">
-                City
-              </label>
-              <div className="w-full h-fit flex justify-between items-center relative">
-                <select
-                  className="pe-10 font-[400] text-[16px] leading-[19px] ps-1 w-[100%] h-[43px] flex justify-between items-center dark:bg-dark1 bg-white rounded-xl border-2 border-grey"
-                  onChange={(e) => {
-                    setAdvanceFilters((prevFilters: any) =>
-                      prevFilters.map((filter: any) =>
-                        filter.key === "city"
-                          ? { ...filter, keyValue: e.target.value }
-                          : filter
-                      )
-                    );
-                  }}
-                  value={
-                    advanceFilters.find((filter: any) => filter.key === "city")
-                      ?.keyValue || ""
-                  }
-                >
-                  <option value="">Select</option>
-                  {Array.from(
-                    new Set(chauffeursData.map((item) => item.data.city))
-                  ).map((city) => (
-                    <option key={city} value={city}>
-                      {city}
-                    </option>
-                  ))}
-                </select>
-                <div className="w-[30px] h-[35px] dark:bg-dark1 bg-white absolute right-1 rounded-xl flex justify-center items-center pointer-events-none">
-                  <img
-                    src={shape.src}
-                    className="w-[10.5px]  dark:filter dark:brightness-[0] dark:invert"
-                  />
-                </div>
-              </div>
-            </div> */}
             <div className="h-[24px] w-full flex justify-end gap-2 items-center font-[400] text-[14px] sm:text-[18px] text-grey">
               <button
                 className="hover:no-underline w-[112px] h-[44px] rounded-[6px] bg-main-blue text-white font-[500] text-[12px] md:text-[18px] flex justify-center items-center leading-[0px]"
