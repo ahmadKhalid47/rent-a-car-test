f814c4d4a24a30c17d77ee9948302bd47bdaa9ef
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-10-29 14:35:23 +0500

    lmklmlm

--- app/AddChauffeur/[chauffeurUpdateAction]/Rental.tsx
+++ app/AddChauffeur/[chauffeurUpdateAction]/Rental.tsx
@@ -35,6 +35,7 @@ import { removing } from "../../Components/functions/removingFileFromDrag";
 import { Thumbs } from "../../Components/functions/thumbsFromDrag";
 import { useFileDrop } from "../../Components/functions/onDragFromDrag";
 import { Country, State, City } from "country-state-city";
+import { FiPlusCircle, FiMinusCircle } from "react-icons/fi";
 
 export default function Rental() {
   let chauffeur = useSelector((state: RootState) => state.chauffeur);
@@ -232,60 +233,85 @@ export default function Rental() {
           <Thumbs files={licfiles} setFiles={setLicFiles} />
         </div>
       </div>
-      <div className="flex flex-wrap justify-start items-start gap-x-[4%] gap-y-5 w-full h-fit dark:bg-dark1 bg-white mt-5 rounded-[10px] border-2 border-grey px-1 xs:px-3 md:px-11 py-8">
-        <span className="flex justify-start gap-4 items-center font-[600] text-[20px] w-full my-1 c">
-          Other Information
-          {/* <span className="font-[400] text-[14px]">Name this field</span> */}
-        </span>
-        <TempTypeInput
-          setState={setotherNumberR}
-          label={"Number"}
-          value={chauffeur.otherNumber}
-          required={false}
-          type={"number"}
-        />
-
-        <TempTypeInput
-          setState={setotherValidR}
-          label={"Valid Until"}
-          value={chauffeur.otherValid}
-          required={false}
-          type={"date"}
-        />
-
-        <TempSelectInput
-          setState={setotherCountryR}
-          label={"Issuing Country/State"}
-          value={chauffeur.otherCountry}
-          required={false}
-          options={countries.map((item: any) => item.label)}
-        />
-        <span className="flex justify-start gap-1 items-center font-[600] text-[20px] w-full mt-1 -mb-2 b">
-          Upload Image*
-        </span>
-
-        <div
-          className="w-full h-[170px] rounded-[12px] border-dashed border-2 flex flex-col justify-center items-center gap-[7px cursor-pointer"
-          {...getRootPropsOther()}
+      <div className="flex justify-start gap-3 items-center w-[100%] mt-5">
+        <button
+          className="flex justify-center gap-3 items-center w-[200px] py-2 md:py-0 h-fit md:h-[44px] rounded-[5px] bg-main-dark-blue text-white  font-[500] text-[12px] md:text-[18px] leading-[21px] text-center"
+          onClick={() => {
+            dispatch(setotherR(!chauffeur.other));
+          }}
         >
-          <input {...getInputPropsOther()} />
-          <img src={upload.src} />
-          <span className="font-[600] text-[12px] xs:text-[13px] md:text-[14px] dark:text-white text-black my-[5px]">
-            Drag & Drop or
-            <span className="text-link-blue cursor-pointer"> choose file </span>
-            to upload
-          </span>
-          <span className="font-[400] text-[14px] leading-[14px] text-[#515978]">
-            Select JPG, PNG
-          </span>
-          <span className="font-[400] text-[14px] leading-[14px] text-[#515978]">
-            Maximum size 5MB
-          </span>
-        </div>
-        <div className="w-full h-fit flex justify-start items-center gap-5 overflow-auto py-[2px]">
-          <Thumbs files={otherfiles} setFiles={setOtherFiles} />
-        </div>
+          {chauffeur.other ? (
+            <>
+              <FiMinusCircle className="text-[25px]" />
+              Cancel
+            </>
+          ) : (
+            <>
+              <FiPlusCircle className="text-[25px]" />
+              Add Other
+            </>
+          )}
+        </button>
       </div>
+      {chauffeur.other && (
+        <div className="flex flex-wrap justify-start items-start gap-x-[4%] gap-y-5 w-full h-fit dark:bg-dark1 bg-white mt-5 rounded-[10px] border-2 border-grey px-1 xs:px-3 md:px-11 py-8">
+          <span className="flex justify-start gap-4 items-center font-[600] text-[20px] w-full my-1 c">
+            Other Information
+            {/* <span className="font-[400] text-[14px]">Name this field</span> */}
+          </span>
+          <TempTypeInput
+            setState={setotherNumberR}
+            label={"Number"}
+            value={chauffeur.otherNumber}
+            required={false}
+            type={"number"}
+          />
+
+          <TempTypeInput
+            setState={setotherValidR}
+            label={"Valid Until"}
+            value={chauffeur.otherValid}
+            required={false}
+            type={"date"}
+          />
+
+          <TempSelectInput
+            setState={setotherCountryR}
+            label={"Issuing Country/State"}
+            value={chauffeur.otherCountry}
+            required={false}
+            options={countries.map((item: any) => item.label)}
+          />
+          <span className="flex justify-start gap-1 items-center font-[600] text-[20px] w-full mt-1 -mb-2 b">
+            Upload Image*
+          </span>
+
+          <div
+            className="w-full h-[170px] rounded-[12px] border-dashed border-2 flex flex-col justify-center items-center gap-[7px cursor-pointer"
+            {...getRootPropsOther()}
+          >
+            <input {...getInputPropsOther()} />
+            <img src={upload.src} />
+            <span className="font-[600] text-[12px] xs:text-[13px] md:text-[14px] dark:text-white text-black my-[5px]">
+              Drag & Drop or
+              <span className="text-link-blue cursor-pointer">
+                {" "}
+                choose file{" "}
+              </span>
+              to upload
+            </span>
+            <span className="font-[400] text-[14px] leading-[14px] text-[#515978]">
+              Select JPG, PNG
+            </span>
+            <span className="font-[400] text-[14px] leading-[14px] text-[#515978]">
+              Maximum size 5MB
+            </span>
+          </div>
+          <div className="w-full h-fit flex justify-start items-center gap-5 overflow-auto py-[2px]">
+            <Thumbs files={otherfiles} setFiles={setOtherFiles} />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
