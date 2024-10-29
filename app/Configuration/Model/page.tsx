f66c4ae0898f4761e43a81f3a747f2327f4f0f14
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-10-29 09:55:45 +0500

    jnknj

--- app/Configuration/Model/page.tsx
+++ app/Configuration/Model/page.tsx
@@ -81,8 +81,8 @@ export default function Vehicles() {
     } else if (
       vehiclesData.find(
         (item) =>
-          item.model?.toLowerCase() === Model.trim().toLowerCase()&&
-          item.make?.toLowerCase() === Make.trim().toLowerCase()&&
+          item.model?.toLowerCase() === Model.trim().toLowerCase() &&
+          item.make?.toLowerCase() === Make.trim().toLowerCase() &&
           item.Category?.toLowerCase() === Category.trim().toLowerCase()
       )
     ) {
@@ -136,6 +136,7 @@ export default function Vehicles() {
     });
     setFilteredVehicles(filtered);
   }
+
   return (
     <div
       className={`${
@@ -199,7 +200,7 @@ export default function Vehicles() {
           </div>
           {popup ? (
             <div className="w-full h-full dark:bg-blackOpacity bg-[rgba(255,255,255,0.9) rounded-[10px] absolute top-[0px] left-0 flex justify-center item-center sm:items-center z-[10]">
-              <div className="w-[90%] sm:w-[600px] h-[430px] border-[1px] border-grey rounded-[10px] mt-0 flex flex-col justify-between items-start gap-x-[4%] gap-y-5 dark:bg-dark1 bg-white shadow-lg z-[15]  py-3 xs:py-5 md:py-14 px-1 xs:px-3 md:px-10 modal-position">
+              <div className="w-[90%] sm:w-[600px] h-[430px] border-[1px] border-grey rounded-[10px] mt-0 flex flex-col justify-between items-start gap-x-[4%] gap-y-5 dark:bg-dark1 bg-white shadow-lg z-[15]  py-3 xs:py-5 md:py-14 px-1 xs:px-3 md:px-10 modal-positio">
                 <div
                   className={`w-[100%] h-fit flex flex-col justify-start items-start gap-1`}
                 >
