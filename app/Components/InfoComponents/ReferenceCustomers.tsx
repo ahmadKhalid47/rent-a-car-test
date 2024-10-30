80e63105d31e8907f37f8dd80b434d222b0a7483
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-10-30 18:09:24 +0500

    dsgsf

--- app/Components/InfoComponents/ReferenceCustomers.tsx
+++ app/Components/InfoComponents/ReferenceCustomers.tsx
@@ -10,6 +10,7 @@ export const ReferenceCustomers: React.FC<InfoComponentProps> = ({
   infoKey,
 }) => {
   const info = useSelector((state: RootState) => state[infoKey][infoKey]);
+  const [refIndex, setRefIndex] = useState(0);
   const [currentIndex, setCurrentIndex] = useState(0);
   console.log(info?.reference);
 
@@ -18,32 +19,60 @@ export const ReferenceCustomers: React.FC<InfoComponentProps> = ({
       <div className="w-[100%] dark:text-white text-black text-[14px] font-[400] flex justify-between items-center">
         <div className="w-fit flex flex-col justify-start item-start">
           <span className="w-full text-[#555555]">Name</span>
-          <span className="w-full">{info?.reference[0]?.refName}</span>
+          <span className="w-full">{info?.reference[refIndex]?.refName}</span>
+        </div>
+        <div className="w-fit flex flex-col justify-start item-start">
+          <span className="w-full text-[#555555]">Address</span>
+          <span className="w-full">{info?.reference[refIndex]?.refAddress}</span>
         </div>
         <div className="w-fit flex flex-col justify-start item-start">
           <span className="w-full text-[#555555]">Phone </span>
-          <span className="w-full">{info?.reference[0]?.refPhone}</span>
+          <span className="w-full">{info?.reference[refIndex]?.refPhone}</span>
         </div>
         <div className="w-fit flex flex-col justify-start item-start">
           <span className="w-full text-[#555555]">Relation</span>
-          <span className="w-full">{info?.reference[0]?.refRelation}</span>
+          <span className="w-full">{info?.reference[refIndex]?.refRelation}</span>
         </div>
       </div>
       <div className="w-[100%] h-fit dark:text-white text-black text-[14px] font-[400] flex justify-between items-center mt-1">
         <div className="relative w-full h-[157px] flex justify-center items-center">
-          {info?.reference[0]?.refImages?.length && (
+          {info?.reference[refIndex]?.refImages?.length && (
             <img
-              src={info?.reference[0]?.refImages[currentIndex]}
+              src={info?.reference[refIndex]?.refImages[currentIndex]}
               className="w-[250px] h-[157px] rounded-[10px]"
             />
           )}
           <button
-            className="absolute left-0 top-1/2 transform -translate-y-1/2 text-2xl p-2"
+            className="absolute left-10 top-1/2 transform -translate-y-1/2 text-2xl p-2"
             onClick={() => {
               setCurrentIndex(
                 (prev) =>
-                  (prev - 1 + info?.reference[0]?.refImages?.length) %
-                  info?.reference[0]?.refImages?.length
+                  (prev - 1 + info?.reference[refIndex]?.refImages?.length) %
+                  info?.reference[refIndex]?.refImages?.length
+              );
+            }}
+          >
+            <FaChevronLeft />
+          </button>
+          <button
+            className="absolute right-10 top-1/2 transform -translate-y-1/2 text-2xl p-2"
+            onClick={() => {
+              setCurrentIndex(
+                (prev) => (prev + 1) % info?.reference[refIndex]?.refImages?.length
+              );
+            }}
+          >
+            <FaChevronRight />
+          </button>
+
+          {/* reference numbering  */}
+          <button
+            className="absolute left-0 top-1/2 transform -translate-y-1/2 text-2xl p-2"
+            onClick={() => {
+              setRefIndex(
+                (prev) =>
+                  (prev - 1 + info?.reference?.length) %
+                  info?.reference?.length
               );
             }}
           >
@@ -52,9 +81,7 @@ export const ReferenceCustomers: React.FC<InfoComponentProps> = ({
           <button
             className="absolute right-0 top-1/2 transform -translate-y-1/2 text-2xl p-2"
             onClick={() => {
-              setCurrentIndex(
-                (prev) => (prev + 1) % info?.reference[0]?.refImages?.length
-              );
+              setRefIndex((prev) => (prev + 1) % info?.reference?.length);
             }}
           >
             <FaChevronRight />
