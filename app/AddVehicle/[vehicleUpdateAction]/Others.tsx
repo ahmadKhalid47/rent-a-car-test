9ebb7398e149ca2ef10b524aa7085bd6105cd9de
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-10-31 11:15:26 +0500

    bgsgbs

--- app/AddVehicle/[vehicleUpdateAction]/Others.tsx
+++ app/AddVehicle/[vehicleUpdateAction]/Others.tsx
@@ -12,14 +12,10 @@ export default function Others() {
   return (
     <div className="w-full h-fit">
       <div className="flex flex-wrap justify-start items-start gap-x-[4%] gap-y-5 w-full h-fit dark:bg-dark1 bg-white mt-5 rounded-[10px] border-2 border-grey px-1 xs:px-3 md:px-11 py-8">
-        <span className="flex justify-start gap-1 items-center font-[600] text-[20px] w-full my-1 c">
-          Others
-        </span>
-
         <div className="w-[100%] h-fit flex flex-col justify-start items-start gap-1">
-          <label className="flex justify-start gap-1 items-start font-[400] text-[14px] leading-[17px]">
+          <span className="flex justify-start gap-1 items-center font-[600] text-[20px] w-full my-1 c">
             Any Additional Notes
-          </label>
+          </span>
           <div className="w-full h-fit flex justify-between items-center relative">
             <textarea
               className="w-full pe-2 py-3 font-[400] text-[16px] leading-[19px] ps-2  flex justify-between items-center dark:bg-dark1 input-color rounded-xl border-2 border-grey"
