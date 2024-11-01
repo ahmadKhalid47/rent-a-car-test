631b1d7b91ef6684cb5bdfcf5780235c2a19a2ba
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-11-01 11:42:29 +0500

    kmlkmklm

--- app/AddVehicle/[vehicleUpdateAction]/Damages.tsx
+++ app/AddVehicle/[vehicleUpdateAction]/Damages.tsx
@@ -1,18 +1,17 @@
 "use client";
-import React from "react";
 import shape from "@/public/ShapeBlack.svg";
 import default_interior from "@/public/car-sedan-interior.svg";
 import default_exterior from "@/public/car-sedan-exterior.svg";
-import { useState, useEffect } from "react";
 import { FaTimesCircle, FaTrash } from "react-icons/fa";
 import { useCallback } from "react";
 import { useDropzone } from "react-dropzone";
 import upload from "@/public/Paper Upload.svg";
-import { useSelector } from "react-redux";
-import { RootState } from "@/app/store";
 import { setdamages, setdamageImagesToDelete } from "@/app/store/Vehicle";
-import { useDispatch } from "react-redux";
 import { setAlert, setSeverity } from "@/app/store/Global";
+import React, { useEffect, useState } from "react";
+import { RootState } from "@/app/store";
+import { useDispatch, useSelector } from "react-redux";
+import { FaAsterisk, FaTimes } from "react-icons/fa";
 
 export default function Damages() {
   let vehicle = useSelector((state: RootState) => state.Vehicle);
@@ -165,7 +164,7 @@ export default function Damages() {
   let interiorImg = Configurations?.Configurations?.type.find(
     (item: any) => item.Type === vehicle.type
   )?.interior;
-console.log(exteriorImg);
+  console.log(damageType);
   return (
     <div className="w-full h-fit">
       <div className="w-full h-fit">
@@ -375,6 +374,7 @@ console.log(exteriorImg);
                 </div>
               </div>
             </div>
+            <Relation value={damageType} action={setDamageType} />
             <div className="w-full sm:w-[181px] h-fit flex flex-col justify-start items-start gap-1">
               <label className="flex justify-start gap-1 items-start truncate font-[400] text-[14px] leading-[17px]">
                 Degree
@@ -468,5 +468,101 @@ console.log(exteriorImg);
         </div>
       ) : null}
     </div>
+  );
+}
+
+export function Relation({ value, action }: any) {
+  let [other2, setOther2] = useState("");
+  let [popUp2, setPopUp2] = useState(false);
+  let [options, setoptions] = useState(
+    !other2
+      ? ["Dent", "Clip", "Scratch", "Other"]
+      : ["Dent", "Clip", "Scratch", value, "Other"]
+  );
+  useEffect(() => {
+    if (value === "Other") setPopUp2(true);
+    setoptions(
+      !other2
+        ? ["Dent", "Clip", "Scratch", "Other"]
+        : ["Dent", "Clip", "Scratch", value, "Other"]
+    );
+  }, [value]);
+
+  return (
+    <>
+      <div className="w-full sm:w-[181px] h-fit flex flex-col justify-start items-start gap-1">
+        <label className="flex justify-start gap-1 items-start truncate font-[400] text-[14px] leading-[17px]">
+          Damage Type
+        </label>
+        <div className="w-full h-fit flex justify-between items-center relative">
+          <select
+            className="pe-10 truncate font-[400] text-[16px] leading-[19px] ps-1 w-[100%] h-[43px] flex justify-between items-center dark:bg-dark1 input-color rounded-xl border-2 border-grey"
+            required={false}
+            value={value}
+            onChange={(e) => action(e.target.value)}
+          >
+            <option value={""}>Select</option>
+            {options?.map((item: any, key: number) => (
+              <option value={item} key={key}>
+                {item ? item : "Select"}
+              </option>
+            ))}
+          </select>
+          <div className="w-[30px] h-[35px] dark:bg-dark1 input-color absolute right-1 rounded-xl flex justify-center items-center pointer-events-none">
+            <img
+              src={shape.src}
+              className="w-[10.5px]  dark:filter dark:brightness-[0] dark:invert"
+            />
+          </div>
+        </div>
+      </div>
+      {popUp2 && (
+        <div className="w-full h-full dark:bg-blackOpacity bg-[rgba(255,255,255,0.9) rounded-[10px] absolute top-[0px] left-0 flex justify-center item-center sm:items-center z-[10]">
+          <div className="w-[90%] sm:w-[600px] h-fit border-[1px] border-grey rounded-[10px] mt-0 flex flex-wrap justify-between items-start gap-x-[4%] gap-y-5 dark:bg-dark1 bg-white z-[15]  py-3 xs:py-5 md:py-14 px-1 xs:px-3 md:px-10 fixed top-[30vh] -translate-y-[50%]">
+            <div
+              className={`w-[100%] h-fit flex flex-col justify-start items-start gap-1`}
+            >
+              <label className="flex justify-start gap-1 items-start font-[600] text-[14px] leading-[17px]">
+                {"Add New"}
+                <FaAsterisk className="text-[6px]" />
+              </label>
+              <div className="w-full h-fit flex justify-between items-center relative">
+                <input
+                  required={true}
+                  type={"text"}
+                  className="pe-10 font-[400] text-[16px] leading-[19px] ps-2 w-[100%] h-[43px] flex justify-between items-center dark:bg-dark1 input-color rounded-xl border-2 border-grey truncate"
+                  placeholder={`Enter Text Here`}
+                  onChange={(e) => {
+                    setOther2(e.target.value);
+                  }}
+                  value={other2}
+                />
+              </div>
+            </div>
+
+            <div className={`w-full flex justify-end gap-4 items-center pt-4`}>
+              <button
+                className="px-2 md:px-0 w-fit py-2 md:py-0 h-fit md:h-[44px] rounded-[10px] dark:bg-dark1 input-color  text-gray-500 font-[400] text-[12px] md:text-[18px] leading-[21px] absolute top-2 right-"
+                onClick={() => {
+                  setOther2("");
+                  setPopUp2(false);
+                }}
+              >
+                <FaTimes />
+              </button>
+              <button
+                className="w-[230px] py-2 md:py-0 h-fit md:h-[44px] rounded-[10px] bg-main-blue text-white  font-[500] text-[12px] xs:text-[14px] md:text-[18px] leading-[21px] text-center"
+                onClick={() => {
+                  action(other2);
+                  setPopUp2(false);
+                }}
+              >
+                Save
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
