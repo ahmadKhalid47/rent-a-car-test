cada80d451355552a34ba4ab00528af5f5ad689c
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-11-04 16:14:41 +0500

    fvsdfvsd

--- app/Components/functions/ExportData.tsx
+++ app/Components/functions/ExportData.tsx
@@ -2,13 +2,17 @@ import { useHandleExport } from "./exportFunction";
 
 const ExcelDownload = ({ data }: any) => {
   const handleExport = useHandleExport();
-  console.log(data);
 
   return (
     <button
       className="hover:no-underline w-[112px] h-[43px] rounded-[6px] bg-main-blue text-white font-[500] text-[12px] md:text-[18px] flex justify-center items-center leading-[0px]"
       onClick={() => {
-        handleExport(data);
+        handleExport(
+          data?.map((item: any) => {
+            const { _id, __v, createdBy, ...rest } = item;
+            return rest;
+          })
+        );
       }}
       disabled={!data}
     >
