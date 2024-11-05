52bdd380f075c72f64ba962bb164cc0e9835c7d4
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-11-05 16:22:04 +0500

    update Damages.tsx

--- app/AddVehicle/[vehicleUpdateAction]/Damages.tsx
+++ app/AddVehicle/[vehicleUpdateAction]/Damages.tsx
@@ -314,30 +314,6 @@ export default function Damages() {
       {popup ? (
         <div className="w-full h-full dark:bg-blackOpacity bg-[rgba(255,255,255,0.9) rounded-[10px] absolute top-0 left-0 flex justify-center item-start sm:items-center z-[10]">
           <div className="w-[90%] sm:w-[500px] h-fit border-[1px] border-grey rounded-[10px] flex flex-wrap justify-between items-start gap-x-[4%] gap-y-5 dark:bg-dark1 bg-white z-[15]  py-3 xs:py-5 md:py-10 px-1 xs:px-3 md:px-10 modal-position modal-animation fixed modal-position modal-animation">
-            <div className="w-full sm:w-[181px] h-fit flex flex-col justify-start items-start gap-1">
-              <label className="flex justify-start gap-1 items-start truncate font-[400] text-[14px] leading-[17px]">
-                Damage Type
-              </label>
-              <div className="w-full h-fit flex justify-between items-center relative">
-                <select
-                  className="pe-10 truncate font-[400] text-[16px] leading-[19px] ps-1 w-[100%] h-[43px] flex justify-between items-center dark:bg-dark1 input-color rounded-xl border-2 border-grey"
-                  onChange={(e) => {
-                    setDamageType(e.target.value);
-                  }}
-                >
-                  <option value="">Select</option>
-                  <option value="Dent">Dent</option>
-                  <option value="Clip">Clip</option>
-                  <option value="Scratch">Scratch</option>
-                </select>
-                <div className="w-[30px] h-[35px] dark:bg-dark1 input-color absolute right-1 rounded-xl flex justify-center items-center pointer-events-none">
-                  <img
-                    src={shape.src}
-                    className="w-[10.5px]  dark:filter dark:brightness-[0] dark:invert"
-                  />
-                </div>
-              </div>
-            </div>
             <Relation value={damageType} action={setDamageType} />
             <div className="w-full sm:w-[181px] h-fit flex flex-col justify-start items-start gap-1">
               <label className="flex justify-start gap-1 items-start truncate font-[400] text-[14px] leading-[17px]">
