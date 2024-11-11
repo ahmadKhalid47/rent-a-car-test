f42fd8c4331634f31793be1f190230f82d6ba788
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-11-11 19:34:29 +0500

    update page.tsx

--- app/UsersInfo/[_id]/page.tsx
+++ app/UsersInfo/[_id]/page.tsx
@@ -19,6 +19,8 @@ import { setUserInfo } from "@/app/store/UserInfo";
 import { TextLoader } from "@/app/Components/Loader";
 import Image from "next/image";
 import { useFetchData } from "@/app/Components/functions/apiCalling";
+import { formatCreatedAtDate } from "@/app/Components/functions/formats";
+import General from "@/app/Components/InfoComponents/General";
 
 export default function UserInfoMainPage() {
   let global = useSelector((state: RootState) => state.Global);
@@ -66,28 +68,28 @@ export default function UserInfoMainPage() {
   useFetchData({
     apiName: "getSortedLeanData",
     modelName: "vehicle",
-    createdBy: UserInfo._id,
+    createdBy: UserInfo?._id,
     setData: setVehiclesData,
     setLoading: setvehicleLoading,
   });
   useFetchData({
     apiName: "getSortedLeanData",
     modelName: "customer",
-    createdBy: UserInfo._id,
+    createdBy: UserInfo?._id,
     setData: setCustomersData,
     setLoading: setCustomerLoading,
   });
   useFetchData({
     apiName: "getSortedLeanData",
     modelName: "chauffeur",
-    createdBy: UserInfo._id,
+    createdBy: UserInfo?._id,
     setData: setChauffeursData,
     setLoading: setChauffeurLoading,
   });
   useFetchData({
     apiName: "getSortedLeanData",
     modelName: "reservation",
-    createdBy: UserInfo._id,
+    createdBy: UserInfo?._id,
     setData: setreservationsData,
     setLoading: setreservationLoading,
   });
@@ -109,8 +111,8 @@ export default function UserInfoMainPage() {
           global.sidebarShow ? "nav-width" : "nav-closed-width"
         } h-fit absolute right-0 flex flex-col justify-start items-start gap-[20px]   pe-[10px] md:pe-[50px] ps-[10px] md:ps-[20px]  pb-14`}
       >
-        <div className="w-full  px-6">
-          <span className="font-[600] text-[25px] leading-[38px] dark:text-white text-black capitalize">
+        <div className="w-full h-[200px">
+          <span className="font-[600] text-[25px] leading-[38px] dark:text-white text-black">
             {UserInfo?.name ? UserInfo?.name : "---"}
           </span>
           <div className="flex justify-between items-start">
@@ -123,175 +125,115 @@ export default function UserInfoMainPage() {
             </span>
           </div>
         </div>
-        <div className="w-full h-fit flex justify-center flex-wrap items-start gap-x-[5%] gap-y-[5%] py-7 px-6 rounded-[10px] mt-5 relative">
-          <div className="w-full h-fit flex justify-start flex-col items-start gap-x-[5%] gap-y-[5%]  rounded-[10px]">
-            <div className="w-full h-fit flex justify-start gap-[7%] items-center dark:bg-dark1 bg-white rounded-[10px] border-2 border-grey py-7 px-6">
-              {/* _______ */}
-              <div className="w-full flex justify-between items-center px-6 py-[20px]">
-                <div className="">
-                  <div className="w-[155px] h-[155px] rounded-2xl ">
-                    <img
-                      alt=""
-                      src={
-                        UserInfo?.profilePic[0]?.includes("http")
-                          ? UserInfo?.profilePic[0]
-                          : image404.src
-                      }
-                      style={{ width: "100%", height: "100%" }}
-                      className="rounded-[10px]"
-                    />
-                  </div>
-                </div>
-                <div className="w-[80%] flex justify-between items-center">
-                  <div className="w-[47%]">
-                    <div className="w-full flex justify-between">
-                      <div className="font-[400] text-[18px]">Full Name:</div>
-                      <div className="font-[400] text-[18px]">
-                        {UserInfo?.name}
-                      </div>
-                    </div>
-                    <div className="border border-t mt-2 mb-2"></div>
-
-                    <div className="w-full flex justify-between">
-                      <div className="font-[400] text-[18px]">Company:</div>
-
-                      <div className="font-[400] text-[18px]">
-                        {UserInfo?.company}
-                      </div>
-                    </div>
-                    <div className="border border-t mt-2 mb-2"></div>
-                    <div className="w-full flex justify-between">
-                      <div className="font-[400] text-[18px]">City:</div>
-                      <div className="font-[400] text-[18px]">
-                        {UserInfo?.city}
-                      </div>
-                    </div>
-                    <div className="border border-t mt-2 mb-2"></div>
-                    <div className="w-full flex justify-between">
-                      <div className="font-[400] text-[18px]">Phone:</div>
-                      <div className="font-[400] text-[18px]">
-                        {UserInfo?.phone}
-                      </div>
-                    </div>
-                  </div>
-                  <div className="w-[47%]">
-                    <div className="w-full flex justify-between">
-                      <div className="font-[400] text-[18px]">Username:</div>
-                      <div className="font-[400] text-[18px]">
-                        {UserInfo?.username}
-                      </div>
-                    </div>
-                    <div className="border border-t mt-2 mb-2"></div>
-
-                    <div className="w-full flex justify-between">
-                      <div className="font-[400] text-[18px]">Country:</div>
-
-                      <div className="font-[400] text-[18px]">
-                        {UserInfo?.country}
-                      </div>
-                    </div>
-                    <div className="border border-t mt-2 mb-2"></div>
-                    <div className="w-full flex justify-between">
-                      <div className="font-[400] text-[18px]">Email:</div>
-                      <div className="font-[400] text-[18px]">
-                        {UserInfo?.email}
-                      </div>
-                    </div>
-                    <div className="border border-t mt-2 mb-2"></div>
-                    <div className="w-full flex justify-between">
-                      <div className="font-[400] text-[18px]">
-                        Plan Validity:
-                      </div>
-                      <div className="font-[400] text-[18px]">
-                        {UserInfo?.plan}
-                      </div>
-                    </div>
-                  </div>
+        <div className="w-full h-fit flex justify-center flex-wrap items-start gap-x-[5%] gap-y-5 py-7 px-6 rounded-[10px] border-2 border-grey dark:bg-dark2 bg-light-grey mt-5 relative overflow-hidden">
+          <div className="w-full h-fit flex justify-start flex-col items-start gap-x-[5%] gap-y-4 rounded-[10px] py-7 border-[1px] border-grey dark:bg-dark1 bg-white px-10">
+            <div className="w-full h-fit flex justify-between items-star rounded-[10px]">
+              <div className="w-[36%] flex justify-center items-center">
+                <div className="w-[267px] h-[267px] flex justify-between items-start rounded-full overflow-hidden border-[1px] border-grey dark:bg-dark1 bg-white">
+                  <img
+                    src={UserInfo?.customerImage || image404.src}
+                    className="w-full h-full"
+                  />
                 </div>
               </div>
-            </div>
-            <div className="w-full h-fit dark:bg-dark1 bg-white  border-2 border-grey mt-5 rounded-[10px] py-10 px-12">
-              <div className="w-full h-fit flex flex-wrap justify-start items-start gap-8 overflow-auto scroll">
-                <div className="w-[290px] h-[100px] flex justify-start flex-wrap items-center gap-x-[5%] gap-y-[5%] ps-4 rounded-[10px] border-2 border-grey dark:bg-dark1 bg-white relative">
-                  <div className="w-[65px] h-[65px] bg-main-blue rounded-[10px] flex justify-center items-center">
-                    <Image alt="" width={40} height={40} src={d7.src} />
+              <div className="w-[64%] flex justify-start flex-col items-start gap-3">
+                <div className="w-full h-fit flex justify-between items-start py-1 border-b-2 border-color">
+                  <div className="flex flex-col justify-start items-start h-fit">
+                    <span className="w-full font-[600] text-[36px] leading-none dark:text-white text-black  mt-[3px]">
+                      {UserInfo?.name}
+                      <div className="font-[400] text-[12px] xs:text-[14px] md:text-[12px] leading-none h-fit py-1">
+                        Created Date: {formatCreatedAtDate(UserInfo?.createdAt)}
+                      </div>{" "}
+                    </span>
                   </div>
-                  <div>
-                    <div className="font-[400] text-[15px] sm:text-[26px] leading-[18px] sm:leading-[39px] h-[39px]">
-                      {!vehicleLoading ? VehiclesData?.length : <TextLoader />}
-                    </div>
-                    <div className="font-[400] text-[15px] sm:text-[18px] leading-[18px] sm:leading-[27px]">
-                      Total Vehicles{" "}
-                    </div>
+                  <div className="flex justify-center items-center w-[150px] h-[48px] bg-light-grey border-[1px] border-light-grey rounded-[3px] overflow-hidden text-center text-[14px] xs:text-[16px] md:text-[21px] font-[500] complete-status">
+                    Active{" "}
                   </div>
                 </div>
-                <div className="w-[290px] h-[100px] flex justify-start flex-wrap items-center gap-x-[5%] gap-y-[5%] ps-4 rounded-[10px] border-2 border-grey dark:bg-dark1 bg-white relative">
-                  <div className="w-[65px] h-[65px] bg-main-blue rounded-[10px] flex justify-center items-center">
-                    <Image alt="" width={40} height={40} src={d3.src} />
+                <div className="w-[100%] dark:text-white text-black  text-[14px] font-[400] flex justify-between items-center">
+                  <div className="w-[45%] flex justify-between items-center">
+                    <span className="dark:text-white text-[#555555]">
+                      Username{" "}
+                    </span>
+                    <span>{UserInfo?.username}</span>
                   </div>
-                  <div>
-                    <div className="font-[400] text-[15px] sm:text-[26px] leading-[18px] sm:leading-[39px] h-[39px]">
-                      {!reservationLoading ? (
-                        reservationsData?.length
-                      ) : (
-                        <TextLoader />
-                      )}
-                    </div>
-                    <div className="font-[400] text-[15px] sm:text-[18px] leading-[18px] sm:leading-[27px]">
-                      Total Reservations{" "}
-                    </div>
+                  <div className="w-[45%] flex justify-between items-center">
+                    <span className="dark:text-white text-[#555555]">
+                      Company
+                    </span>
+                    <span>{UserInfo?.company || "---"}</span>
                   </div>
                 </div>
-                <div className="w-[290px] h-[100px] flex justify-start flex-wrap items-center gap-x-[5%] gap-y-[5%] ps-4 rounded-[10px] border-2 border-grey dark:bg-dark1 bg-white relative">
-                  <div className="w-[65px] h-[65px] bg-main-blue rounded-[10px] flex justify-center items-center">
-                    <Image alt="" width={40} height={40} src={d4.src} />
+                <div className="w-[100%] dark:text-white text-black  text-[14px] font-[400] flex justify-between items-center">
+                  <div className="w-[45%] flex justify-between items-center">
+                    <span className="dark:text-white text-[#555555]">
+                      Phone
+                    </span>
+                    <span>{UserInfo?.phone}</span>
                   </div>
-                  <div>
-                    <div className="font-[400] text-[15px] sm:text-[26px] leading-[18px] sm:leading-[39px] h-[39px]">
-                      {!CustomerLoading ? (
-                        CustomersData?.length
-                      ) : (
-                        <TextLoader />
-                      )}
-                    </div>
-                    <div className="font-[400] text-[15px] sm:text-[18px] leading-[18px] sm:leading-[27px]">
-                      Total Customers{" "}
-                    </div>
+                  <div className="w-[45%] flex justify-between items-center">
+                    <span className="dark:text-white text-[#555555]">
+                      Email
+                    </span>
+                    <span>{UserInfo?.email}</span>
                   </div>
-                </div>
-                <div className="w-[290px] h-[100px] flex justify-start flex-wrap items-center gap-x-[5%] gap-y-[5%] ps-4 rounded-[10px] border-2 border-grey dark:bg-dark1 bg-white relative">
-                  <div className="w-[65px] h-[65px] bg-main-blue rounded-[10px] flex justify-center items-center">
-                    <Image alt="" width={40} height={40} src={d2.src} />
+                </div>{" "}
+                <div className="w-[100%] dark:text-white text-black  text-[14px] font-[400] flex justify-between items-center">
+                  <div className="w-[45%] flex justify-between items-center">
+                    <span className="dark:text-white text-[#555555]">
+                      State/Province
+                    </span>
+                    <span>{UserInfo?.state}</span>
                   </div>
-                  <div>
-                    <div className="font-[400] text-[15px] sm:text-[26px] leading-[18px] sm:leading-[39px] h-[39px]">
-                      {!ChauffeurLoading ? (
-                        ChauffeursData?.length
-                      ) : (
-                        <TextLoader />
-                      )}
-                    </div>
-                    <div className="font-[400] text-[15px] sm:text-[18px] leading-[18px] sm:leading-[27px]">
-                      Total Chauffeurs{" "}
-                    </div>
+                  <div className="w-[45%] flex justify-between items-center">
+                    <span className="dark:text-white text-[#555555]">City</span>
+                    <span>{UserInfo?.city}</span>
                   </div>
                 </div>
-                <div className="w-[290px] h-[100px] flex justify-start flex-wrap items-center gap-x-[5%] gap-y-[5%] ps-4 rounded-[10px] border-2 border-grey dark:bg-dark1 bg-white relative">
-                  <div className="w-[65px] h-[65px] bg-main-blue rounded-[10px] flex justify-center items-center">
-                    <Image alt="" width={40} height={40} src={d1.src} />
+                <div className="w-[100%] dark:text-white text-black  text-[14px] font-[400] flex justify-between items-center">
+                  <div className="w-[45%] flex justify-between items-center">
+                    <span className="dark:text-white text-[#555555]">
+                      No. of Vehicles
+                    </span>
+                    <span>{UserInfo?.noOfVehicles || "---"}</span>
                   </div>
-                  <div>
-                    <div className="font-[400] text-[15px] sm:text-[26px] leading-[18px] sm:leading-[39px] h-[39px]">
-                      {!reservationLoading ? (
-                        `${global.currentCurrency} ` +
-                        totalAmount.toLocaleString("en-US")
-                      ) : (
-                        <TextLoader />
-                      )}
-                    </div>
-                    <div className="font-[400] text-[15px] sm:text-[18px] leading-[18px] sm:leading-[27px]">
-                      Total Revenue
-                    </div>
+                  <div className="w-[45%] flex justify-between items-center">
+                    <span className="dark:text-white text-[#555555]">
+                      Country
+                    </span>
+                    <span>{UserInfo?.country}</span>
+                  </div>
+                </div>
+                <div className="w-[100%] dark:text-white text-black  text-[14px] font-[400] flex justify-between items-center bg-yellow-00 -mt-[6px]">
+                  <div className="w-[45%] flex justify-between items-center">
+                    <span className="dark:text-white text-[#555555]">
+                      Last Renewal Date
+                    </span>
+                    <span>21-10-2024</span>
+                  </div>
+                  <div className="w-[45%] flex justify-between items-center">
+                    <span className="dark:text-white text-[#555555]">
+                      Validity Plan
+                    </span>
+                    <span className="w-[60px] h-[27px] flex justify-center items-center border-[1px] border-grey rounded-[3px] bg-light-grey">
+                      1 Month
+                    </span>
+                  </div>
+                </div>
+                <div className="w-[100%] dark:text-white text-black  text-[14px] font-[400] flex justify-between items-center bg-yellow-00 -mt-[6px]">
+                  <div className="w-[45%] flex justify-between items-center">
+                    <span className="dark:text-white text-[#555555]">
+                      Renewal Date
+                    </span>
+                    <span>21-10-2024</span>
+                  </div>
+                  <div className="w-[45%] flex justify-between items-center">
+                    <span className="dark:text-white text-[#555555]">
+                      Days to Expire
+                    </span>
+                    <span className="w-[60px] h-[27px] flex justify-center items-center border-[1px] border-grey rounded-[3px] bg-light-grey">
+                      30
+                    </span>
                   </div>
                 </div>
               </div>
