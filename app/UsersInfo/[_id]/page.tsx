826cf46d7e832b909137193d9b9bd31ea02678b2
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-11-11 19:48:42 +0500

    update page.tsx

--- app/UsersInfo/[_id]/page.tsx
+++ app/UsersInfo/[_id]/page.tsx
@@ -4,6 +4,7 @@ import d3 from "@/public/dashboard (3).svg";
 import d7 from "@/public/dashboard (7).svg";
 import d4 from "@/public/ad (1).svg";
 import d2 from "@/public/ad (2).svg";
+import arrows from "@/public/arrows.svg";
 import React from "react";
 import { RootState } from "@/app/store";
 import { useSelector } from "react-redux";
@@ -21,6 +22,9 @@ import Image from "next/image";
 import { useFetchData } from "@/app/Components/functions/apiCalling";
 import { formatCreatedAtDate } from "@/app/Components/functions/formats";
 import General from "@/app/Components/InfoComponents/General";
+import { MdEmail } from "react-icons/md";
+import { PaginationComponent } from "@/app/Components/functions/Pagination";
+import { GoTriangleDown } from "react-icons/go";
 
 export default function UserInfoMainPage() {
   let global = useSelector((state: RootState) => state.Global);
@@ -34,8 +38,27 @@ export default function UserInfoMainPage() {
     }
   }, [isMobile]);
   const params = useParams(); // Get all route parameters
+  const [page, setPage] = useState(1);
   const { _id } = params;
+  const itemsPerPage = 12;
   const [loading, setLoading] = useState<any>(true);
+    const data: any = [
+      {
+        data: {
+          name: "John Smith",
+          username: "username",
+          email: "example@email.com",
+          companyDate: "Basic",
+          expiryDate: "dd/mm/yyyy",
+          daysRemaining: "10 Days",
+        },
+      },
+    ];
+
+  const [sortedData, setSortedData] = useState<any>(data);
+  const handleChange = (event: any, value: any) => {
+    setPage(value);
+  };
 
   let { UserInfo } = useSelector((state: RootState) => state.UserInfo);
 
@@ -103,7 +126,12 @@ export default function UserInfoMainPage() {
     (sum: any, record: any) => sum + Number(record.data.amount),
     0
   );
+  const totalPages = Math.ceil(sortedData?.length / itemsPerPage);
 
+  const paginatedData = sortedData.slice(
+    (page - 1) * itemsPerPage,
+    page * itemsPerPage
+  );
   return (
     <div className="w-fit h-fit mt-[90px] pt-5">
       <div
@@ -235,6 +263,156 @@ export default function UserInfoMainPage() {
                       30
                     </span>
                   </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-full h-fit flex justify-between items-start">
+            <div className="h-fit flex flex-col justify-start items-start gap-x-[4%] gap-y-5 w-full dark:bg-dark1 bg-white">
+              <div className="w-[100%] flex justify-start items-start flex-col border-[1px] border-light-grey rounded-[16px]">
+                <div className="w-[100%] flex justify-between items-center pt-6 px-5 -mb-4">
+                  <span className="flex flex-col justify-between font-[600] text-[16px] xs:text-[18px] md:text-[20px] leading-none dark:text-white text-black">
+                    Revenue Summary
+                  </span>
+                  <div className="w-[100%] sm:w-[200px] h-fit flex flex-col justify-start items-start gap-1 dark:text-white text-black me-7">
+                    <div className="w-full h-fit flex justify-between items-center relative">
+                      <select className="pe-6 font-[400] text-[14px] leading-[19px] ps-1 w-[100%] h-[43px] flex justify-between items-center dark:bg-dark1 bg-white rounded-xl border-[1px] border-grey">
+                        <option value="">Jan 2024 - Jun 2024</option>
+                        <option value="">July 2024 - Dec 2024</option>
+                      </select>
+                      <div className="w-[30px] h-[35px] dark:bg-dark1 bg-white absolute right-1 rounded-xl flex justify-center items-center pointer-events-none">
+                        <GoTriangleDown className="text-[18px]" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="w-full h-fit mt-2">
+                  <div className="w-full h-fit overflow-auto rounded-[10px] border-[1px] border-grey relative">
+                    <div className="w-[900px] 1200:w-full h-fit flex flex-col justify-start items-start dark:bg-dark2 bg-light-grey overflow-hidden mt-0 leading-[17px]">
+                      <div className="w-full h-[43px] flex justify-between items-center font-[600] text-[12px] sm:text-[14px] rounded-t-[10px] text-center border-b-2 border-grey">
+                        <div className="text-start pe-3 flex justify-start gap-3 items-center w-[14%] 1 ps-5 cursor-pointer">
+                          Date{" "}
+                          <Image
+                            alt=""
+                            width={10}
+                            height={10}
+                            src={arrows.src}
+                            className="cursor-pointer hover:ring-8 rounded-full hover:bg-gray-200 ring-gray-200"
+                          />
+                        </div>
+                        <div className="text-start pe-3 flex justify-start gap-3 items-center w-[12%] 2 cursor-pointer">
+                          No. of Vehicles{" "}
+                          <Image
+                            alt=""
+                            width={10}
+                            height={10}
+                            src={arrows.src}
+                            className="cursor-pointer hover:ring-8 rounded-full hover:bg-gray-200 ring-gray-200"
+                          />
+                        </div>
+                        <div className="text-start pe-3 flex justify-start gap-3 items-center w-[18%] 3 cursor-pointer">
+                          Validity Plan{" "}
+                          <Image
+                            alt=""
+                            width={10}
+                            height={10}
+                            src={arrows.src}
+                            className="cursor-pointer hover:ring-8 rounded-full hover:bg-gray-200 ring-gray-200"
+                          />
+                        </div>
+                        <div className="text-start pe-3 flex justify-start gap-3 items-center w-[12%] 4 cursor-pointer">
+                          Renewal Date{" "}
+                          <Image
+                            alt=""
+                            width={10}
+                            height={10}
+                            src={arrows.src}
+                            className="cursor-pointer hover:ring-8 rounded-full hover:bg-gray-200 ring-gray-200"
+                          />
+                        </div>
+                        <div className="text-start pe-3 flex justify-start gap-3 items-center w-[14%] 6 cursor-pointer">
+                          Discount{" "}
+                          <Image
+                            alt=""
+                            width={10}
+                            height={10}
+                            src={arrows.src}
+                            className="cursor-pointer hover:ring-8 rounded-full hover:bg-gray-200 ring-gray-200"
+                          />
+                        </div>
+                        <div className="text-start pe-3 flex justify-start gap-3 items-center w-[14%] 6 cursor-pointer">
+                          Total Amount{" "}
+                          <Image
+                            alt=""
+                            width={10}
+                            height={10}
+                            src={arrows.src}
+                            className="cursor-pointer hover:ring-8 rounded-full hover:bg-gray-200 ring-gray-200"
+                          />
+                        </div>
+                        <div className="text-start pe-3 flex justify-start gap-3 items-center w-[14%] 6 cursor-pointer">
+                          Status{" "}
+                          <Image
+                            alt=""
+                            width={10}
+                            height={10}
+                            src={arrows.src}
+                            className="cursor-pointer hover:ring-8 rounded-full hover:bg-gray-200 ring-gray-200"
+                          />
+                        </div>
+                        <div className="text-start flex justify-start items-center w-[10%] pe-5 5">
+                          Download
+                        </div>
+                      </div>
+                      {paginatedData?.length < 1 ? (
+                        <span className="p-3">No Vehicles found.</span>
+                      ) : (
+                        paginatedData.map((item: any, index: number) => (
+                          <div key={index} className="w-full">
+                            <div
+                              className={`w-full h-[43px] flex justify-between items-center font-[400] text-[12px] sm:text-[14px] text-center capitalize dark:bg-dark1 bg-white border-b-2 border-grey`}
+                            >
+                              <div className="text-start pe-3 break-words w-[14%] 1 ps-5">
+                                {item?.data?.name}
+                              </div>
+                              <div className="text-start pe-3 break-words w-[12%] 2">
+                                {item?.data?.username}
+                              </div>
+                              <div className="text-start pe-3 break-words w-[18%] 3">
+                                {item?.data?.email}
+                              </div>
+                              <div className="text-start pe-3 break-words w-[12%] 4">
+                                {item?.data?.companyDate}
+                              </div>
+                              <div className="text-start pe-3 break-words w-[14%] 6">
+                                {item?.data?.expiryDate}
+                              </div>
+                              <div className="text-start pe-3 break-words w-[14%] 6">
+                                {item?.data?.daysRemaining}
+                              </div>
+                              <div
+                                className="flex justify-start items-center w-[10%] pe-5 5 h-full gap-1"
+                                onClick={(event) => {
+                                  event.preventDefault();
+                                  event.stopPropagation();
+                                }}
+                              >
+                                <span className="bg-main-blue text-white px-2 py-[2px] text-[12px] rounded-[5px]">
+                                  Renew
+                                </span>
+                                <MdEmail className="text-main-dark-blue hover:scale-[1.3] cursor-pointer text-[18px]" />
+                              </div>
+                            </div>
+                          </div>
+                        ))
+                      )}
+                    </div>
+                  </div>
+                  <PaginationComponent
+                    totalPages={totalPages}
+                    page={page}
+                    handleChange={handleChange}
+                  />
                 </div>
               </div>
             </div>
