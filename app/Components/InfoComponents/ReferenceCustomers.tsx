f3c45a545222e53a9f4cb8c3140281ded0219e17
ahmadKhalid47 <ahmadrazakhalid9.0@gmail.com>
2024-11-26 15:10:41 +0500

    update ReferenceCustomers.tsx

--- app/Components/InfoComponents/ReferenceCustomers.tsx
+++ app/Components/InfoComponents/ReferenceCustomers.tsx
@@ -16,37 +16,54 @@ export const ReferenceCustomers: React.FC<InfoComponentProps> = ({
   return (
     <div className="w-full h-full py-3 px-5 flex justify-start flex-col items-start gap-1 overflow-auto">
       <div className="w-[100%] dark:text-white text-black text-[14px] font-[400] flex justify-between items-center">
-        <div className="w-fit flex flex-col justify-start item-start">
-          <span className="w-full text-[#555555]">Name</span>
-          <span className="w-full">{info?.reference[refIndex]?.refName}</span>
+        <button
+          className="text-3xl hover:opacity-75"
+          onClick={() => {
+            setRefIndex(
+              (prev) =>
+                (prev - 1 + info?.reference?.length) % info?.reference?.length
+            );
+          }}
+        >
+          <FaChevronLeft />
+        </button>
+        <div className="w-[80%] dark:text-white text-black text-[14px] font-[400] flex flex-col justify-between items-start">
+          <div className="w-full flex justify-between item-start gap-2">
+            <span className="w-fit text-[#555555]">Name</span>
+            <span className="w-fit">{info?.reference[refIndex]?.refName}</span>
+          </div>
+          <div className="w-full flex justify-between item-start gap-2">
+            <span className="w-fit text-[#555555]">Address</span>
+            <span className="w-fit">
+              {info?.reference[refIndex]?.refAddress}
+            </span>
+          </div>
+          <div className="w-full flex justify-between item-start gap-2">
+            <span className="w-fit text-[#555555]">Phone </span>
+            <span className="w-fit">{info?.reference[refIndex]?.refPhone}</span>
+          </div>
+          <div className="w-full flex justify-between item-start gap-2">
+            <span className="w-fit text-[#555555]">Relation</span>
+            <span className="w-fit">
+              {info?.reference[refIndex]?.refRelation}
+            </span>
+          </div>
         </div>
-        <div className="w-fit flex flex-col justify-start item-start">
-          <span className="w-full text-[#555555]">Address</span>
-          <span className="w-full">
-            {info?.reference[refIndex]?.refAddress}
-          </span>
-        </div>
-        <div className="w-fit flex flex-col justify-start item-start">
-          <span className="w-full text-[#555555]">Phone </span>
-          <span className="w-full">{info?.reference[refIndex]?.refPhone}</span>
-        </div>
-        <div className="w-fit flex flex-col justify-start item-start">
-          <span className="w-full text-[#555555]">Relation</span>
-          <span className="w-full">
-            {info?.reference[refIndex]?.refRelation}
-          </span>
-        </div>
+        <button
+          className="text-3xl hover:opacity-75"
+          onClick={() => {
+            setRefIndex((prev) => (prev + 1) % info?.reference?.length);
+          }}
+        >
+          <FaChevronRight />
+        </button>
       </div>
       <div className="w-[100%] h-fit dark:text-white text-black text-[14px] font-[400] flex justify-between items-center mt-2">
-        <div className="relative w-full h-[157px] flex justify-center items-center">
-          {info?.reference[refIndex]?.refImages?.length > 0 && (
+        {info?.reference[refIndex]?.refImages?.length > 0 && (
+          <div className="relative w-full h-[157px] flex justify-center items-center">
             <>
-              <img
-                src={info?.reference[refIndex]?.refImages[currentIndex]}
-                className="relative w-[250px] h-[157px] rounded-[10px]"
-              />
               <button
-                className="absolute left-10 top-1/2 transform -translate-y-1/2 text-2xl p-2"
+                className="text-2xl hover:opacity-75 mx-2"
                 onClick={() => {
                   setCurrentIndex(
                     (prev) =>
@@ -59,8 +76,12 @@ export const ReferenceCustomers: React.FC<InfoComponentProps> = ({
               >
                 <FaChevronLeft />
               </button>
+              <img
+                src={info?.reference[refIndex]?.refImages[currentIndex]}
+                className="relative w-[250px] h-[157px] rounded-[10px]"
+              />
               <button
-                className="absolute right-10 top-1/2 transform -translate-y-1/2 text-2xl p-2"
+                className="text-2xl hover:opacity-75 mx-2"
                 onClick={() => {
                   setCurrentIndex(
                     (prev) =>
@@ -71,33 +92,8 @@ export const ReferenceCustomers: React.FC<InfoComponentProps> = ({
                 <FaChevronRight />
               </button>
             </>
-          )}
-
-          {info?.reference[refIndex]?.refImages?.length > 0 && (
-            <>
-              <button
-                className="absolute left-0 top-1/2 transform -translate-y-1/2 text-3xl p-2"
-                onClick={() => {
-                  setRefIndex(
-                    (prev) =>
-                      (prev - 1 + info?.reference?.length) %
-                      info?.reference?.length
-                  );
-                }}
-              >
-                <FaChevronLeft />
-              </button>
-              <button
-                className="absolute right-0 top-1/2 transform -translate-y-1/2 text-3xl p-2"
-                onClick={() => {
-                  setRefIndex((prev) => (prev + 1) % info?.reference?.length);
-                }}
-              >
-                <FaChevronRight />
-              </button>
-            </>
-          )}
-        </div>
+          </div>
+        )}
       </div>
     </div>
   );
